f1945ed8c7afa75bda509fc65c16e05041efa971
DaveOps <daveops@DaveOpss-MBP.lan>
2022-03-02 22:32:43 +0200

    Jenkins test

--- .ci/scripts/unit-test.sh
+++ .ci/scripts/unit-test.sh
@@ -8,8 +8,8 @@ jenkins_setup
 make update
 mage goTestUnit
 
-OUT_FILE="build/TEST-go-unit"
-if [ -n "${TEST_COVERAGE}" ] && [ -f "${OUT_FILE}.cov" ]; then
-  go install -modfile=tools/go.mod github.com/t-yuki/gocover-cobertura
-  gocover-cobertura < "${OUT_FILE}.cov" > "${OUT_FILE}_cov.xml"
-fi
+# OUT_FILE="build/TEST-go-unit"
+# if [ -n "${TEST_COVERAGE}" ] && [ -f "${OUT_FILE}.cov" ]; then
+#   go install -modfile=tools/go.mod github.com/t-yuki/gocover-cobertura
+#   gocover-cobertura < "${OUT_FILE}.cov" > "${OUT_FILE}_cov.xml"
+# fi
