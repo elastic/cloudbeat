d80fc7f1b53508013cbe33adc0823ff465f93ed6
orenzohar <oren.zohar@elastic.co>
2022-03-06 12:15:54 +0200

    don't use CustomizePackaging

--- magefile.go
+++ magefile.go
@@ -88,7 +88,6 @@ func Package() {
 	defer func() { fmt.Println("package ran for", time.Since(start)) }()
 
 	devtools.UseElasticBeatOSSPackaging()
-	cloudbeat.CustomizePackaging()
 
 	if packageTypes := os.Getenv("TYPES"); packageTypes != "" {
 		filterPackages(packageTypes)
