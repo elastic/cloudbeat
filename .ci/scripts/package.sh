cc4f2c48b50c15413947c53881bfbc05556238e8
David Natachanny <David.Natachanny@elastic.co>
2022-03-08 17:19:36 +0200

    Platforms

--- .ci/scripts/package.sh
+++ .ci/scripts/package.sh
@@ -3,6 +3,6 @@ set -euox pipefail
 
 # linux/amd64 is in the default list already, set here
 # to prevent jenkins_release.sh from adding more PLATFORMS
-export PLATFORMS="${PLATFORMS:-+linux/amd64}"
+export PLATFORMS="linux/amd64"
 
 make release
