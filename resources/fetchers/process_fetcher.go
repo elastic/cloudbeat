645e54332b111cafec34ccc3a9ca4fb11bc87855
Ofir Lapid <ofiriro3@gmail.com>
2022-03-09 14:04:40 +0200

    Adding config file reader to process fetcher

--- resources/fetchers/process_fetcher.go
+++ resources/fetchers/process_fetcher.go
@@ -2,18 +2,27 @@ package fetchers
 
 import (
 	"context"
-
+	"errors"
+	"fmt"
+	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/elastic/beats/v7/x-pack/osquerybeat/ext/osquery-extension/pkg/proc"
+	"k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
 const (
-	ProcessType = "process"
+	ProcessType        = "process"
+	CMDArgumentMatcher = "\\b%s=(\\S+)"
 )
 
 type ProcessResource struct {
-	PID  string        `json:"pid"`
-	Cmd  string        `json:"command"`
-	Stat proc.ProcStat `json:"stat"`
+	PID    string                 `json:"pid"`
+	Cmd    string                 `json:"command"`
+	Stat   proc.ProcStat          `json:"stat"`
+	Config map[string]interface{} `json:"config"`
 }
 
 type ProcessesFetcher struct {
@@ -22,7 +31,12 @@ type ProcessesFetcher struct {
 
 type ProcessFetcherConfig struct {
 	BaseFetcherConfig
-	Directory string `config:"directory"` // parent directory of target procfs
+	Directory         string                               `config:"directory"` // parent directory of target procfs
+	RequiredProcesses map[string]ProcessInputConfiguration `config:"required_processes"`
+}
+
+type ProcessInputConfiguration struct {
+	RequiredFileInputArguments []string `config:"required-file-arguments"`
 }
 
 func NewProcessesFetcher(cfg ProcessFetcherConfig) Fetcher {
@@ -42,20 +56,79 @@ func (f *ProcessesFetcher) Fetch(ctx context.Context) ([]FetchedResource, error)
 	// If errors occur during read, then return what we have till now
 	// without reporting errors.
 	for _, p := range pids {
-		cmd, err := proc.ReadCmdLine(f.cfg.Directory, p)
-		if err != nil {
-			return ret, nil
-		}
-
 		stat, err := proc.ReadStat(f.cfg.Directory, p)
 		if err != nil {
-			return ret, nil
+			return nil, err
+		}
+		processInput, isProcessRequired := f.cfg.RequiredProcesses[stat.Name]
+		if !isProcessRequired {
+			continue
 		}
 
-		ret = append(ret, ProcessResource{PID: p, Cmd: cmd, Stat: stat})
+		fetchedResource, err := f.fetchProcessData(stat, processInput, p)
+		if err != nil {
+			logp.Error(fmt.Errorf("%+v", err))
+			continue
+		}
+		ret = append(ret, fetchedResource)
 	}
 
 	return ret, nil
+}
+
+func (f *ProcessesFetcher) fetchProcessData(procStat proc.ProcStat, process ProcessInputConfiguration, processId string) (FetchedResource, error) {
+	cmd, err := proc.ReadCmdLine(f.cfg.Directory, processId)
+	if err != nil {
+		return nil, err
+	}
+
+	configMap := f.getProcessFileConfiguration(process, cmd, procStat.Name)
+
+	return ProcessResource{PID: processId, Cmd: cmd, Stat: procStat, Config: configMap}, nil
+}
+
+func (f *ProcessesFetcher) getProcessFileConfiguration(process ProcessInputConfiguration, cmd string, processName string) map[string]interface{} {
+	configMap := make(map[string]interface{}, 0)
+	for _, config := range process.RequiredFileInputArguments {
+		regex := fmt.Sprintf(CMDArgumentMatcher, config)
+		matcher := regexp.MustCompile(regex)
+		if !matcher.MatchString(cmd) {
+			logp.Error(fmt.Errorf("failed to find argument %s to process %s", config, processName))
+			continue
+		}
+		configPath := filepath.Join(f.cfg.Directory, matcher.FindStringSubmatch(cmd)[1])
+		data, err := os.ReadFile(configPath)
+		if err != nil {
+			logp.Error(fmt.Errorf("failed to read file configuration for process %s, error - %+v", processName, err))
+			continue
+		}
+		configFile, err := f.readConfigurationFile(configPath, data)
+		if err != nil {
+			logp.Error(fmt.Errorf("failed to parse file configuration for process %s, error - %+v", processName, err))
+			continue
+		}
+		configMap[config] = configFile
+	}
+	return configMap
+}
+
+func (f *ProcessesFetcher) readConfigurationFile(path string, data []byte) (interface{}, error) {
+	ext := filepath.Ext(path)
+	var output interface{}
+
+	switch ext {
+	case ".json":
+		if err := json.Unmarshal(data, &output); err != nil {
+			return nil, err
+		}
+	case ".yaml":
+		if err := yaml.Unmarshal(data, &output); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, errors.New("can't parse data")
+	}
+	return output, nil
 }
 
 func (f *ProcessesFetcher) Stop() {
