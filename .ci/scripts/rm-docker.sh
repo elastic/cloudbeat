e7e2f456108a1cf73720b95efafc9fc099652a00
David Natachanny <David.Natachanny@elastic.co>
2022-03-21 17:02:34 +0200

    update rm-docker sh

--- .ci/scripts/rm-docker.sh
+++ .ci/scripts/rm-docker.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
-set -euox pipefail
-
 IMAGE="docker.elastic.co/infra/release-manager:latest"
-WORKFLOW="staging"
+WORKFLOW="snapshot"
+VERSION="8.2.0"
+BRANCH="main"
+
+# Allow other users write access to create checksum files
+chmod -R 777 ${BASE_DIR}/build/distributions 
 
 # The "branch" here selects which "$BRANCH.gradle" file of release manager is used
 # VERSION=$(make get-version)
-VERSION="8.2.0"
-MAJOR=$(echo $VERSION | awk -F. '{ print $1 }')
-MINOR=$(echo $VERSION | awk -F. '{ print $2 }')
-if [ -n "$(git ls-remote --heads origin $MAJOR.$MINOR)" ] ; then
-    BRANCH=$MAJOR.$MINOR
-elif [ -n "$(git ls-remote --heads origin $MAJOR.x)" ] ; then
-    BRANCH=$MAJOR.x
-else
-    BRANCH=main
-fi
+# MAJOR=$(echo $VERSION | awk -F. '{ print $1 }')
+# MINOR=$(echo $VERSION | awk -F. '{ print $2 }')
+# if [ -n "$(git ls-remote --heads origin $MAJOR.$MINOR)" ] ; then
+#     BRANCH=$MAJOR.$MINOR
+# elif [ -n "$(git ls-remote --heads origin $MAJOR.x)" ] ; then
+#     BRANCH=$MAJOR.x
+# else
+#     BRANCH=main
+# fi
+
 
 # Generate checksum files and upload to GCS
 docker run --rm \
@@ -23,12 +26,11 @@ docker run --rm \
   -e VAULT_ADDR \
   -e VAULT_ROLE_ID \
   -e VAULT_SECRET_ID \
-  --mount type=bind,readonly=false,src="$PWD",target=/artifacts \
+  --mount type=bind,readonly=false,src="$PWD/${BASE_DIR}",target=/artifacts \
   "$IMAGE" \
     cli collect \
       --project cloudbeat \
       --branch "$BRANCH" \
       --commit `git rev-parse HEAD` \
       --workflow "$WORKFLOW" \
-      # --qualifier "$VERSION_QUALIFIER" \
       --artifact-set main
