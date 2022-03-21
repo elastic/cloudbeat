d315435028889cf84ac59b07943d3fe2f93482b2
orenzohar <oren.zohar@elastic.co>
2022-03-21 17:19:05 +0200

    add AS2 licensing throughout our project

--- magefile.go
+++ magefile.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 //go:build mage
 // +build mage
 
@@ -26,7 +43,6 @@ import (
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/test"
 
 	"github.com/elastic/beats/v7/dev-tools/mage/gotool"
-	"go.uber.org/multierr"
 )
 
 func init() {
@@ -151,17 +167,9 @@ func AddLicenseHeaders() error {
 
 	licenser := gotool.Licenser
 
-	return multierr.Combine(
-		licenser(
-			licenser.License("ASL2"),
-			licenser.Exclude("x-pack"),
-			licenser.Exclude("generator/_templates/beat/{beat}"),
-			licenser.Exclude("generator/_templates/metricbeat/{beat}"),
-		),
-		licenser(
-			licenser.License("Elastic"),
-			licenser.Path("x-pack"),
-		),
+	return licenser(
+		licenser.License("ASL2"),
+		licenser.Exclude("x-pack"),
 	)
 }
 
@@ -174,19 +182,9 @@ func CheckLicenseHeaders() error {
 
 	licenser := gotool.Licenser
 
-	return multierr.Combine(
-		licenser(
-			licenser.Check(),
-			licenser.License("ASL2"),
-			licenser.Exclude("x-pack"),
-			licenser.Exclude("generator/_templates/beat/{beat}"),
-			licenser.Exclude("generator/_templates/metricbeat/{beat}"),
-		),
-		licenser(
-			licenser.Check(),
-			licenser.License("Elastic"),
-			licenser.Path("x-pack"),
-		),
+	return licenser(
+		licenser.Check(),
+		licenser.License("ASL2"),
 	)
 }
 
