4dbbb39c3d8b540617b9323cae5b50021b680c51
Ofir Lapid <ofiriro3@gmail.com>
2022-03-21 17:10:17 +0200

    remove prefix validation

--- resources/fetchers/process_factory.go
+++ resources/fetchers/process_factory.go
@@ -2,12 +2,10 @@ package fetchers
 
 import (
 	"encoding/gob"
-	"fmt"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/cloudbeat/resources/fetching"
 	"github.com/elastic/cloudbeat/resources/manager"
 	"os"
-	"strings"
 )
 
 const (
@@ -36,9 +34,6 @@ func (f *ProcessFactory) Create(c *common.Config) (fetching.Fetcher, error) {
 		return nil, err
 	}
 
-	if !strings.HasPrefix(cfg.Directory, "/hostfs") {
-		return nil, fmt.Errorf("process fetcher could not start - the directory path should start with `/hostfs`")
-	}
 	cfg.Fs = os.DirFS(cfg.Directory)
 	return f.CreateFrom(cfg)
 }
