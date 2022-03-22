78e98f64bde8ce78e46f88eb438da935bdea66cf
Oren Zohar <85433724+oren-zohar@users.noreply.github.com>
2022-03-22 11:29:14 +0200

    Add ASL2 licensing (#24)

--- magefile.go
+++ magefile.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 //go:build mage
 // +build mage
 
@@ -14,6 +31,7 @@ import (
 	"github.com/magefile/mage/mg"
 
 	devtools "github.com/elastic/beats/v7/dev-tools/mage"
+
 	cloudbeat "github.com/elastic/cloudbeat/scripts/mage"
 	// mage:import
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/pkg"
@@ -23,6 +41,8 @@ import (
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/integtest/notests"
 	// mage:import
 	_ "github.com/elastic/beats/v7/dev-tools/mage/target/test"
+
+	"github.com/elastic/beats/v7/dev-tools/mage/gotool"
 )
 
 func init() {
@@ -130,6 +150,42 @@ func filterPackages(types string) {
 // TestPackages tests the generated packages (i.e. file modes, owners, groups).
 func TestPackages() error {
 	return devtools.TestPackages()
+}
+
+// Fmt formats code and adds license headers.
+func Fmt() {
+	mg.Deps(devtools.GoImports, devtools.PythonAutopep8)
+	mg.Deps(AddLicenseHeaders)
+}
+
+// AddLicenseHeaders adds ASL2 headers to .go files outside of x-pack and
+// add Elastic headers to .go files in x-pack.
+func AddLicenseHeaders() error {
+	fmt.Println(">> fmt - go-licenser: Adding missing headers")
+
+	mg.Deps(devtools.InstallGoLicenser)
+
+	licenser := gotool.Licenser
+
+	return licenser(
+		licenser.License("ASL2"),
+		licenser.Exclude("x-pack"),
+	)
+}
+
+// CheckLicenseHeaders checks ASL2 headers in .go files outside of x-pack and
+// checks Elastic headers in .go files in x-pack.
+func CheckLicenseHeaders() error {
+	fmt.Println(">> fmt - go-licenser: Checking for missing headers")
+
+	mg.Deps(devtools.InstallGoLicenser)
+
+	licenser := gotool.Licenser
+
+	return licenser(
+		licenser.Check(),
+		licenser.License("ASL2"),
+	)
 }
 
 func Update() { mg.Deps(cloudbeat.Update.All) }
