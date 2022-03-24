4b36abd3d5a87c228d2f912b4a60211f6d2fc27b
Evgeniy Belyi <jeniawhite92@gmail.com>
2022-03-24 17:05:37 +0200

    Removing deep copy of state in data.go (#26)
    * Removing deepcopy data state
    
    * Dereference pointers of k8s data resources

--- resources/fetchers/kube_factory.go
+++ resources/fetchers/kube_factory.go
@@ -18,8 +18,6 @@
 package fetchers
 
 import (
-	"encoding/gob"
-
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/libbeat/common/kubernetes"
 	"github.com/elastic/cloudbeat/resources/fetching"
@@ -31,18 +29,6 @@ type KubeFactory struct {
 
 func init() {
 	manager.Factories.ListFetcherFactory(fetching.KubeAPIType, &KubeFactory{})
-	gob.Register(K8sResource{})
-	gob.Register(ECRResource{})
-	gob.Register(ELBResource{})
-	gob.Register(EKSResource{})
-	gob.Register(IAMResource{})
-	gob.Register(kubernetes.Pod{})
-	gob.Register(kubernetes.Role{})
-	gob.Register(kubernetes.RoleBinding{})
-	gob.Register(kubernetes.ClusterRole{})
-	gob.Register(kubernetes.ClusterRoleBinding{})
-	gob.Register(kubernetes.NetworkPolicy{})
-	gob.Register(kubernetes.PodSecurityPolicy{})
 }
 
 func (f *KubeFactory) Create(c *common.Config) (fetching.Fetcher, error) {
