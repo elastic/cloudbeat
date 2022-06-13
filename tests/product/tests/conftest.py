42a78d926212d497f9a039ee66bf8d7a10e0af9d
Oren Zohar <85433724+oren-zohar@users.noreply.github.com>
2022-06-13 23:33:46 +0300

    Kube api rules automated tests (#169)
    * 5.1.5, 5.1.6, 5.2.2, 5.2.3, 5.2.4, 5.2.5, 5.2.6, 5.2.7
    
    * test env changes
    
    * timeout fix + justfile commands
    
    * remove unused function
    
    * fix merge conflicts
    
    Co-authored-by: Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>

--- tests/product/tests/conftest.py
+++ tests/product/tests/conftest.py
@@ -2,11 +2,12 @@ from pathlib import Path
 import time
 import pytest
 from kubernetes.client import ApiException
-
+from kubernetes.utils import FailToCreateError
+import json
 from commonlib.io_utils import get_k8s_yaml_objects
 
 DEPLOY_YML = "../../deploy/cloudbeat-pytest.yml"
-BUSYBOX_POD_YML = "../../deploy/mock-pod.yml"
+KUBE_RULES_ENV_YML = "../../deploy/mock-pod.yml"
 POD_RESOURCE_TYPE = "Pod"
 
 
@@ -41,35 +42,55 @@ def config_node_pre_test(data):
 
 
 @pytest.fixture(scope='module')
-def setup_busybox_pod(data):
+def clean_test_env(data):
     """
-    Creates busybox pod to play with
+    Sets up a testing env with needed kube resources
     """
-    file_path = Path(__file__).parent / BUSYBOX_POD_YML
     k8s_client, api_client, cloudbeat_agent = data
-    k8s_yaml_list = get_k8s_yaml_objects(file_path=file_path)
-    pod_name = k8s_yaml_list[0]['Pod']['name']
 
-    # check if we already have one - delete if so
-    try:
-        # try getting the pod before deleting it - will raise exception if not found
-        k8s_client.get_resource(resource_type=POD_RESOURCE_TYPE, name=pod_name, namespace=cloudbeat_agent.namespace)
-        k8s_client.delete_from_yaml(get_k8s_yaml_objects(file_path=file_path))
-        deleted = k8s_client.wait_for_resource(
-            resource_type=POD_RESOURCE_TYPE,
-            name=pod_name,
-            namespace=cloudbeat_agent.namespace,
-            status="DELETED")
-        print(f"busybox deleted: {deleted}")
-    except ApiException as notFound:
-        print(f"no busybox online - setting up a new one: {notFound}")
+    file_path = Path(__file__).parent / KUBE_RULES_ENV_YML
+    k8s_resources = get_k8s_yaml_objects(file_path=file_path)
 
-    # create busybox
-    k8s_client.create_from_yaml(
-        yaml_file=file_path,
-        namespace=cloudbeat_agent.namespace,
-        verbose=True
-    )
+    for yml_resource in k8s_resources:
+        # check if we already have one - delete if so
+        resource_type, metadata = yml_resource['kind'], yml_resource['metadata']
+        relevant_metadata = {k: metadata[k] for k in ('name', 'namespace') if k in metadata}
+        try:
+            # try getting the resource before deleting it - will raise exception if not found
+            k8s_client.get_resource(resource_type=resource_type, **relevant_metadata)
+            k8s_client.delete_resources(resource_type=resource_type, **relevant_metadata)
+            deleted = k8s_client.wait_for_resource(resource_type=resource_type, status_list=["DELETED"], **relevant_metadata)
+            print(f"{resource_type} deleted: {deleted}")
+        except ApiException as notFound:
+            print(f"no {relevant_metadata['name']} online - setting up a new one: {notFound}")
+            # create resource
+            k8s_client.create_from_dict(data=yml_resource, **relevant_metadata)
+
     yield k8s_client, api_client, cloudbeat_agent
     # teardown
-    k8s_client.delete_from_yaml(yaml_objects_list=k8s_yaml_list)
+    k8s_client.delete_from_yaml(yaml_objects_list=k8s_resources)
+
+
+@pytest.fixture(scope='module')
+def test_env(data):
+    """
+    Sets up a testing env with needed kube resources
+    """
+    k8s, api_client, cloudbeat_agent = data
+
+    file_path = Path(__file__).parent / KUBE_RULES_ENV_YML
+    k8s_resources = get_k8s_yaml_objects(file_path=file_path)
+
+    try:
+        k8s.create_from_yaml(yaml_file=file_path, namespace=cloudbeat_agent.namespace)
+    except FailToCreateError as conflict:
+        print([json.loads(c.body)['message'] for c in conflict.api_exceptions])
+
+    for yml_resource in k8s_resources:
+        resource_type, metadata = yml_resource['kind'], yml_resource['metadata']
+        relevant_metadata = {k: metadata[k] for k in ('name', 'namespace') if k in metadata}
+        k8s.wait_for_resource(resource_type=resource_type, status_list=["RUNNING", "ADDED"], **relevant_metadata)
+
+    yield k8s, api_client, cloudbeat_agent
+    # teardown
+    k8s.delete_from_yaml(yaml_objects_list=k8s_resources)  # stop agent
