e1dd3ce3c4624ecb3343ef900d432d1d2a6c68c2
Ofir Lapid <ofiriro3@gmail.com>
2022-07-07 19:35:28 +0300

    Update according to review

--- scripts/remote_edit_config.sh
+++ scripts/remote_edit_config.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-ARCH=`uname -a | rev | cut -d " " -f 1 | rev` # This should return arm64 on m1 and x86_64 on regular mac
+ARCH=$(uname -a | rev | cut -d " " -f 1 | rev) # This should return arm64 on m1 and x86_64 on regular mac
 PREFIX="/usr/share/elastic-agent/data/"
-SUFFIX="/install/cloudbeat-8.3.0-SNAPSHOT-linux-${ARCH}/cloudbeat.yml"
+CLOUDBEAT_VERSION=$(grep defaultBeatVersion ../cmd/version.go | cut -d'=' -f2 | tr -d '" ')
+SUFFIX="/install/cloudbeat-${CLOUDBEAT_VERSION}-SNAPSHOT-linux-${ARCH}/cloudbeat.yml"
 TMP_LOCAL="/tmp/cloudbeat.yml"
 
-PODS=`kubectl -n kube-system get pod -l app=elastic-agent -o name`
+PODS=$(kubectl -n kube-system get pod -l app=elastic-agent -o name)
 for P in $PODS
 do
-    POD=`echo $P | cut -d '/' -f 2`
-    FOLDER=`kubectl -n kube-system exec $POD -- ls $PREFIX`
+    POD=$(echo $P | cut -d '/' -f 2)
+    FOLDER=$(kubectl -n kube-system exec $POD -- ls $PREFIX)
     FULL_PATH="${PREFIX}${FOLDER}${SUFFIX}"
-    kubectl -n kube-system cp $POD:$FULL_PATH $TMP_LOCAL
+    kubectl -n kube-system cp "$POD":"$FULL_PATH" $TMP_LOCAL
     vi $TMP_LOCAL
-    kubectl -n kube-system cp $TMP_LOCAL $POD:$FULL_PATH
-    kubectl -n kube-system exec $POD -- chmod go-w $FULL_PATH
-    kubectl -n kube-system exec $POD -- chown root:root $FULL_PATH
+    kubectl -n kube-system cp $TMP_LOCAL "$POD":"$FULL_PATH"
+    kubectl -n kube-system exec "$POD" -- chmod go-w "$FULL_PATH"
+    kubectl -n kube-system exec "$POD" -- chown root:root "$FULL_PATH"
     rm $TMP_LOCAL
 done
 
--- scripts/remote_replace_cloudbeat.sh
+++ scripts/remote_replace_cloudbeat.sh
@@ -5,19 +5,20 @@
 # b. The running elastic-agents were built with "DEV=TRUE" flag (https://github.com/elastic/security-team/blob/main/docs/cloud-security-posture-team/Onboarding/cloudbeat-ec2.md#:~:text=Build%20the%20agent%20(We%20are%20building%20with%20DEV%3Dtrue%2C%20so%20we%20can%20load%20cloudbeat.tar.gz%20without%20signature%20verification))
 
 #!/bin/bash
-ARCH=`uname -a | rev | cut -d " " -f 1 | rev` # This should return arm64 on m1 and x86_64 on regular mac
+ARCH=$(uname -a | rev | cut -d " " -f 1 | rev) # This should return arm64 on m1 and x86_64 on regular mac
 PREFIX="/usr/share/elastic-agent/data/"
-SUFFIX="/install/cloudbeat-8.4.0-SNAPSHOT-linux-${ARCH}"
+CLOUDBEAT_VERSION=$(grep defaultBeatVersion ../cmd/version.go | cut -d'=' -f2 | tr -d '" ')
+CLOUDBEAT_IMAGE="cloudbeat-${CLOUDBEAT_VERSION}-SNAPSHOT-linux-${ARCH}"
+SUFFIX="/install/${CLOUDBEAT_IMAGE}"
 DISTRIBUTION_LOCAL_FOLDER="../build/distributions"
-CLOUDBEAT_IMAGE="cloudbeat-8.4.0-SNAPSHOT-linux-$ARCH"
 
-PODS=`kubectl -n kube-system get pod -l app=elastic-agent -o name`
+PODS=$(kubectl -n kube-system get pod -l app=elastic-agent -o name)
 for P in $PODS
 do
-    POD=`echo $P | cut -d '/' -f 2`
-    FOLDER=`kubectl -n kube-system exec $POD -- ls $PREFIX`
+    POD=$(echo $P | cut -d '/' -f 2)
+    FOLDER=$(kubectl -n kube-system exec $POD -- ls $PREFIX)
     # Kill Cloudbeat process
-    CLOUDBEAT_PID=`kubectl -n kube-system exec "$POD" -- pidof cloudbeat`
+    CLOUDBEAT_PID=$(kubectl -n kube-system exec "$POD" -- pidof cloudbeat)
     kubectl -n kube-system exec "$POD" -- kill -9 "${CLOUDBEAT_PID}"
     # Copy Cloudbeat from your local distribution folder to the agent
     kubectl -n kube-system cp ${DISTRIBUTION_LOCAL_FOLDER}/"${CLOUDBEAT_IMAGE}".tar.gz "$POD":/usr/share/elastic-agent/state/data/downloads/"${CLOUDBEAT_IMAGE}".tar.gz
