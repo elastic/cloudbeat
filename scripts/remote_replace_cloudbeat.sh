25622aac889b57a8879e5aa71afe6abd57c9a656
Ofir Lapid <ofiriro3@gmail.com>
2022-07-12 18:57:25 +0300

    Review changes

--- scripts/remote_replace_cloudbeat.sh
+++ scripts/remote_replace_cloudbeat.sh
@@ -14,32 +14,25 @@ DISTRIBUTION_LOCAL_FOLDER="../build/distributions"
 DOWNLOAD_PATH="/usr/share/elastic-agent/state/data/downloads/"
 
 PODS=$(kubectl -n kube-system get pod -l app=elastic-agent -o name)
-for P in $PODS
-do
-    POD=$(echo $P | cut -d '/' -f 2)
-    FOLDER=$(kubectl -n kube-system exec $POD -- ls $PREFIX)
-    # Kill Cloudbeat process
-    CLOUDBEAT_PID=$(kubectl -n kube-system exec "$POD" -- pidof cloudbeat)
-    kubectl -n kube-system exec "$POD" -- kill -9 "${CLOUDBEAT_PID}"
-    # Copy Cloudbeat from your local distribution folder to the agent
-    kubectl -n kube-system cp ${DISTRIBUTION_LOCAL_FOLDER}/"${CLOUDBEAT_IMAGE}".tar.gz "$POD":"${DOWNLOAD_PATH}""${CLOUDBEAT_IMAGE}".tar.gz
-    kubectl -n kube-system cp ${DISTRIBUTION_LOCAL_FOLDER}/"${CLOUDBEAT_IMAGE}".tar.gz.sha512 "$POD":${DOWNLOAD_PATH}"${CLOUDBEAT_IMAGE}".tar.gz.sha512
-
-    # Delete Cloudbeat artifacts
-    INSTALLATION_DIRECTORY=${PREFIX}${FOLDER}"/install"
-    kubectl -n kube-system exec "$POD" -- rm -r "${INSTALLATION_DIRECTORY}/${CLOUDBEAT_IMAGE}"
-    # Create new Cloudbeat artifcats
-    kubectl -n kube-system exec "$POD" -- tar xf "${DOWNLOAD_PATH}""${CLOUDBEAT_IMAGE}".tar.gz -C "${INSTALLATION_DIRECTORY}"
-    kubectl -n kube-system exec "$POD" -- chown -R elastic-agent:elastic-agent "${INSTALLATION_DIRECTORY}"
-    kubectl -n kube-system exec "$POD" -- chown -R root:root "${INSTALLATION_DIRECTORY}"/"${CLOUDBEAT_IMAGE}"/cloudbeat.yml
-    kubectl -n kube-system exec "$POD" -- chmod 0744 "${INSTALLATION_DIRECTORY}"/"${CLOUDBEAT_IMAGE}"/cloudbeat.yml
-#    chown -R root:root "${INSTALLATION_DIRECTORY}"/*/*.yml && \
-#    chmod 0644 "${INSTALLATION_DIRECTORY}"/*/*.yml && \
-#    "${INSTALLATION_DIRECTORY}"/"${CLOUDBEAT_IMAGE}"/cloudbeat -E setup.ilm.enabled=false -E setup.template.enabled=false -E management.enabled
+for P in $PODS; do
+  POD=$(echo $P | cut -d '/' -f 2)
+  FOLDER=$(kubectl -n kube-system exec $POD -- ls $PREFIX)
+  # Copy Cloudbeat from your local distribution folder to the agent
+  kubectl -n kube-system cp ${DISTRIBUTION_LOCAL_FOLDER}/"${CLOUDBEAT_IMAGE}".tar.gz "$POD":"${DOWNLOAD_PATH}""${CLOUDBEAT_IMAGE}".tar.gz
+  kubectl -n kube-system cp ${DISTRIBUTION_LOCAL_FOLDER}/"${CLOUDBEAT_IMAGE}".tar.gz.sha512 "$POD":${DOWNLOAD_PATH}"${CLOUDBEAT_IMAGE}".tar.gz.sha512
+  # Delete Cloudbeat artifacts
+  INSTALLATION_DIRECTORY=${PREFIX}${FOLDER}"/install"
+  kubectl -n kube-system exec "$POD" -- rm -r "${INSTALLATION_DIRECTORY}/${CLOUDBEAT_IMAGE}"
+  # Create new Cloudbeat artifcats
+  kubectl -n kube-system exec "$POD" -- tar xf "${DOWNLOAD_PATH}""${CLOUDBEAT_IMAGE}".tar.gz -C "${INSTALLATION_DIRECTORY}"
+  # Kill the Cloudbeat process
+  CLOUDBEAT_PID=$(kubectl -n kube-system exec "$POD" -- pidof cloudbeat)
+  kubectl -n kube-system exec "$POD" -- kill -9 "${CLOUDBEAT_PID}"
 done
 
-# After the script finishes its work, you need to make Cloudbeat run again.
-# This can be achieved by:
+# After the script finishes its work, sometimes you need to manually run Cloudbeat again.
+# You can ssh to the pod and run `ps aux | grep cloudbeat`
+# if you see no results please follow the instructions below:
 # a. Open Kibana and press the menu button on the top left of the screen.
 # b. Select "Fleet".
 # c. Press the "agent policies" tab and select the relevant policy.
