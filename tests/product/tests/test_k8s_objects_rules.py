678e82654ee3ebd816d598c7cb466a025f473b09
Yash Tewari <yashtewari1996@gmail.com>
2022-07-28 19:57:53 +0530

    Fix broken k8s_object_rules tests. (#302)
    Most of the problems occur in resource-matching logic that tries to find the right ES event to compare the expected evaluation against, which is harder to do for K8S resources than other resource types.
    
    Inserting a per-testcase UUID into the K8S object's metadata that is being tested allows for much simpler resource-matching than the recursion logic in the `dict_contains` utility.

--- tests/product/tests/test_k8s_objects_rules.py
+++ tests/product/tests/test_k8s_objects_rules.py
@@ -3,12 +3,12 @@ Kubernetes CIS rules verification.
 This module verifies correctness of retrieved findings by manipulating audit and remediation actions
 """
 from datetime import datetime
+import uuid
 
 import pytest
 
-# from product.tests.data.k8s_object.k8s_object_rules import *
 from product.tests.data.k8s_object import k8s_object_rules as k8s_tc
-from commonlib.utils import get_ES_evaluation, get_resource_identifier
+from commonlib.utils import get_ES_evaluation
 from commonlib.framework.reporting import skip_param_case, SkipReportData
 
 
@@ -22,22 +22,15 @@ from commonlib.framework.reporting import skip_param_case, SkipReportData
         *k8s_tc.cis_5_2_3.values(),
         *k8s_tc.cis_5_2_4.values(),
         *k8s_tc.cis_5_2_5.values(),
-        *skip_param_case(skip_list=[*k8s_tc.cis_5_1_5_skip.values(),
-                                    *k8s_tc.cis_5_2_2.values(),
-                                    *k8s_tc.cis_5_2_3_skip.values(),
-                                    *k8s_tc.cis_5_2_4_skip.values(),
-                                    *k8s_tc.cis_5_2_5_skip.values(),
-                                    *k8s_tc.cis_5_2_6.values(),
-                                    *k8s_tc.cis_5_2_7.values(),
-                                    *k8s_tc.cis_5_2_8.values()
-                                    ],
+        *k8s_tc.cis_5_2_2.values(),
+        *k8s_tc.cis_5_2_6.values(),
+        *k8s_tc.cis_5_2_8.values(),
+        *skip_param_case(skip_list=[*k8s_tc.cis_5_2_7.values()],
                          data_to_report=SkipReportData(
-                             url_title="security-team: #4312",
-                             url_link="https://github.com/elastic/security-team/issues/4312",
-                             skip_reason="known issue: broken k8s object tests"
+                             url_title="security-team: #4540",
+                             url_link="https://github.com/elastic/security-team/issues/4540",
+                             skip_reason="Known issue: incorrect implementation"
                          ))
-        # *k8s_tc.cis_5_2_9.values(), - TODO: cases are not implemented
-        # *k8s_tc.cis_5_2_10.values() - TODO: cases are not implemented
     ],
     ids=[
         *k8s_tc.cis_5_1_3.keys(),
@@ -46,11 +39,7 @@ from commonlib.framework.reporting import skip_param_case, SkipReportData
         *k8s_tc.cis_5_2_3.keys(),
         *k8s_tc.cis_5_2_4.keys(),
         *k8s_tc.cis_5_2_5.keys(),
-        *k8s_tc.cis_5_1_5_skip.keys(),
         *k8s_tc.cis_5_2_2.keys(),
-        *k8s_tc.cis_5_2_3_skip.keys(),
-        *k8s_tc.cis_5_2_4_skip.keys(),
-        *k8s_tc.cis_5_2_5_skip.keys(),
         *k8s_tc.cis_5_2_6.keys(),
         *k8s_tc.cis_5_2_7.keys(),
         *k8s_tc.cis_5_2_8.keys(),
@@ -82,6 +71,11 @@ def test_kube_resource_patch(elastic_client, test_env, rule_tag, resource_type, 
 
     assert resource, f"Resource {resource_type} not found"
 
+    test_resource_id = str(uuid.uuid4())
+
+    labels = metadata.setdefault('labels', {})
+    labels['test_resource_id'] = test_resource_id
+
     # patch resource
     resource = k8s_client.patch_resources(
         resource_type=resource_type,
@@ -92,13 +86,19 @@ def test_kube_resource_patch(elastic_client, test_env, rule_tag, resource_type, 
         raise ValueError(
             f'Could not patch resource type {resource_type}:'
             f' {relevant_metadata} with patch {resource_body}')
-    
+
+    def match_resource(eval_resource):
+        try:
+            return eval_resource.metadata.labels.test_resource_id == test_resource_id
+        except AttributeError:
+            return False
+
     evaluation = get_ES_evaluation(
         elastic_client=elastic_client,
         timeout=agent_config.findings_timeout,
         rule_tag=rule_tag,
         exec_timestamp=datetime.utcnow(),
-        resource_identifier=get_resource_identifier(resource_body),
+        resource_identifier=match_resource,
     )
 
     assert evaluation is not None, f"No evaluation for rule {rule_tag} could be found"
