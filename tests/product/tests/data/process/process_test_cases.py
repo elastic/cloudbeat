a2f75216f8275b746e83bceaeb2da398bcf0a564
Yash Tewari <yashtewari1996@gmail.com>
2022-09-20 08:27:56 +0530

    Resource identifier for process_* test suites.

--- tests/product/tests/data/process/process_test_cases.py
+++ tests/product/tests/data/process/process_test_cases.py
@@ -1363,18 +1363,14 @@ scheduler_rules = [
 ]
 
 kubelet_rules = [
-    *skip_param_case(skip_list=[*cis_4_2_1,
-                                *cis_4_2_2,
-                                *cis_4_2_3,
-                                *cis_4_2_4,
-                                *cis_4_2_5,
-                                *cis_4_2_6,
-                                *cis_4_2_7,
-                                *cis_4_2_9,
-                                ],
-                     data_to_report=SkipReportData(
-                         skip_reason="Dangling tests"
-                     )),
+    *cis_4_2_1,
+    *cis_4_2_2,
+    *cis_4_2_3,
+    *cis_4_2_4,
+    *cis_4_2_5,
+    *cis_4_2_6,
+    *cis_4_2_7,
+    *cis_4_2_9,
     # *cis_4_2_8, # TODO setting is not configurable via the Kubelet config file.
     *cis_4_2_10,
     *cis_4_2_11,
