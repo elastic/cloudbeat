7707204d28dbcebe569692df9d3844a0cd3edd72
David Natachanny <David.Natachanny@elastic.co>
2022-11-03 19:44:18 +0200

    load image just agent test

--- .ci/scripts/kind-images.sh
+++ .ci/scripts/kind-images.sh
@@ -2,4 +2,17 @@
 set -euxo pipefail
 IMAGE_SUFFIX=$1
 CLUSTER_NAME=$2
-kind load image-archive /tmp/cloudbeat-${IMAGE_SUFFIX}.tar --name=$CLUSTER_NAME & kind load image-archive /tmp/pytest-${IMAGE_SUFFIX}.tar --name=$CLUSTER_NAME & kind load image-archive /tmp/elastic-agent-${IMAGE_SUFFIX}.tar --name=$CLUSTER_NAME
+IMAGES="cloudbeat pytest elastic-agent"
+
+load_image () {
+  kind load image-archive /tmp/"$1"-"${IMAGE_SUFFIX}".tar --name="$CLUSTER_NAME"
+}
+
+# for i in $IMAGES
+# do
+#   load_image "$i"
+# done
+
+# load_image cloudbeat &
+# load_image pytest &
+load_image elastic-agent
