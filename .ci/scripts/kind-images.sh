593eabe7ca878c976d701c33752e8165947a68a3
David Natachanny <David.Natachanny@elastic.co>
2022-11-06 13:52:40 +0200

    kind-images script add agent image

--- .ci/scripts/kind-images.sh
+++ .ci/scripts/kind-images.sh
@@ -2,17 +2,11 @@
 set -euxo pipefail
 IMAGE_SUFFIX=$1
 CLUSTER_NAME=$2
-IMAGES="cloudbeat pytest elastic-agent"
 
 load_image () {
   kind load image-archive /tmp/"$1"-"${IMAGE_SUFFIX}".tar --name="$CLUSTER_NAME"
 }
 
-# for i in $IMAGES
-# do
-#   load_image "$i"
-# done
-
-# load_image cloudbeat &
-# load_image pytest &
+load_image cloudbeat &
+load_image pytest &
 load_image elastic-agent
