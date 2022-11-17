29346ad689bc25ebfff0717a361aa521e505217c
Amir Ben Nun <34831306+amirbenun@users.noreply.github.com>
2022-11-17 17:59:30 +0200

    Fix v2 bundle path (#511)
    (cherry picked from commit df95c5dcfbbf2aead94e2213582163409c60bf6a)

--- evaluator/opa.go
+++ evaluator/opa.go
@@ -21,7 +21,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"path/filepath"
 	"time"
 
 	"github.com/elastic/cloudbeat/config"
@@ -67,15 +66,11 @@ var logPlugin = `
 func NewOpaEvaluator(ctx context.Context, log *logp.Logger, cfg config.Config) (Evaluator, error) {
 	// provide the OPA configuration which specifies
 	// fetching policy bundle and logging decisions locally to the console
-	path, err := filepath.Abs("bundle.tar.gz")
-	log.Debugf("OPA bundle path: %s", path)
 
-	if err != nil {
-		return nil, err
-	}
+	log.Infof("OPA bundle path: %s", cfg.BundlePath)
 
 	plugin := fmt.Sprintf(logPlugin, dlogger.PluginName, dlogger.PluginName)
-	opaCfg := fmt.Sprintf(opaConfig, path, plugin)
+	opaCfg := fmt.Sprintf(opaConfig, cfg.BundlePath, plugin)
 
 	decisonLogger := newLogger()
 	stdLogger := newLogger()
@@ -101,6 +96,7 @@ func NewOpaEvaluator(ctx context.Context, log *logp.Logger, cfg config.Config) (
 		log.Warn("no runtime config supplied")
 	}
 
+	log.Info("Successfully initiated OPA")
 	return &OpaEvaluator{
 		log:            log,
 		opa:            opa,
