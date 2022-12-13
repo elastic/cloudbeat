eecd91233ff73f43dd9c84877267b92e27ece29a
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2022-12-13 16:46:28 +0200

    update ecr-images script

--- .ci/scripts/ecr-images.sh
+++ .ci/scripts/ecr-images.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 IMAGE_SUFFIX=$1
 ECR_REGISTRY=$2
+DEFAULT_IMAGE_TAG="${DEFAULT_IMAGE_TAG:-$(make get-version)-SNAPSHOT}"
 
 
 load_and_push_image () {
@@ -11,4 +12,5 @@ load_and_push_image () {
 }
 
 load_and_push_image cloudbeat "cloudbeat:latest" &
-load_and_push_image pytest "cloudbeat-test:latest"
+load_and_push_image pytest "cloudbeat-test:latest" &
+load_and_push_image elastic-agent "elastic-agent:$DEFAULT_IMAGE_TAG"
