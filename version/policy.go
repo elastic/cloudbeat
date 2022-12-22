62c8ad6c9f5bda280676c80e64601f4a9639ccc5
Evgeniy Belyi <jeniawhite92@gmail.com>
2022-12-22 11:24:53 +0200

    Bumping the policy version

--- version/policy.go
+++ version/policy.go
@@ -17,7 +17,7 @@
 
 package version
 
-const policyVersion = "v1.2.3"
+const policyVersion = "v1.2.4"
 
 // PolicyVersion returns cloudbeat version info used for the build.
 func PolicyVersion() Version {
