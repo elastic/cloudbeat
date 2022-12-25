1a81c97d1f0ccdf5759467838b6908b712110b77
eyalkraft <63912106+eyalkraft@users.noreply.github.com>
2022-12-25 10:00:52 +0200

    Add tf run agent on eks (#582)
    * add eks
    
    * rename
    
    * initial
    
    * fix comments
    
    * add -target doc
    
    * works
    
    * better
    
    * WORKS
    
    * fix readme
    
    * perfect
    
    * add monitoring
    
    * more
    
    * progress
    
    * works after restarting deamonset
    
    * perfect
    
    * improve README

---
+++ deploy/cloud/modules/api/outputs.tf
@@ -0,0 +1,35 @@
+output "agent_policy_id" {
+  description = "Agent policy ID"
+  value       = restapi_object.agent_policy.id
+}
+
+output "enrollment_token" {
+  description = "Agent enrollment token"
+  value       = local.enrollment_token
+}
+
+output "fleet_url" {
+  description = "Fleet Server URL"
+  value       = local.fleet_url
+}
+
+output "yaml" {
+  description = "Kubernetes deployment YAML"
+  value       = local.yaml
+}
+
+output "manifests" {
+  description = "Kubernetes deployment hcl manifests"
+  value       = local.manifests
+}
+
+output "other_manifests" {
+  description = "Kubernetes deployment hcl manifests of all the resources but the service account"
+  value       = local.other_manifests
+}
+
+output "service_account_manifests" {
+  description = "Kubernetes deployment hcl manifests of the service account(s)"
+  value       = local.service_account_manifests
+}
+
---
+++ deploy/cloud/modules/api/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    restapi = {
+      source = "mastercard/restapi"
+      version = "~> 1.18.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "~> 3.2.1"
+    }
+  }
+
+  required_version = ">= 1.3, <2.0.0"
+}
