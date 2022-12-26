8b20594bd1fdbbb5efc80f677a733979ec5459f5
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2022-12-26 17:41:28 +0200

    update variable

--- .ci/scripts/ecr-images.sh
+++ .ci/scripts/ecr-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 IMAGE_SUFFIX=$1
-ECR_REGISTRY=$2
+CONTAINER_REGISTRY=$2
 ELASTIC_AGENT_DOCKER_TAG=$(make get-ci-agent-version)
 
 
@@ -9,9 +9,9 @@ ELASTIC_AGENT_DOCKER_TAG=$(make get-ci-agent-version)
 load_and_push_image () {
     docker load -i /tmp/"$1"-"${IMAGE_SUFFIX}".tar
     if [ $1 != "elastic-agent" ]; then
-        docker tag "$2" $ECR_REGISTRY/"$2"
+        docker tag "$2" $CONTAINER_REGISTRY/"$2"
     fi
-    docker push $ECR_REGISTRY/"$2"
+    docker push $CONTAINER_REGISTRY/"$2"
 }
 
 load_and_push_image cloudbeat "cloudbeat:latest" &
