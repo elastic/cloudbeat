58adeae92b4493bbe259e92bfdedb92203288409
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2023-01-12 15:24:16 +0200

    update lint issues

--- tests/commonlib/elastic_wrapper.py
+++ tests/commonlib/elastic_wrapper.py
@@ -3,7 +3,6 @@ This module is a wrapper of ElasticSearch low-level client
 """
 
 from elasticsearch import Elasticsearch
-from loguru import logger
 
 
 class ElasticWrapper:
@@ -61,8 +60,7 @@ class ElasticWrapper:
         """
         try:
             ret_value = data["hits"]["hits"][index]["_source"]
-        except IndexError as ex:
-            # logger.warning(ex)
+        except IndexError:
             return {}
         return ret_value
 
