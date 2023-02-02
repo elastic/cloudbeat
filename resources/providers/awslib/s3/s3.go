24271cacb15ff1284e60098094938825d3c45c38
uri-weisman <uri.weisman@elastic.co>
2023-02-02 15:30:38 +0200

    modify s3 provider

--- resources/providers/awslib/s3/s3.go
+++ resources/providers/awslib/s3/s3.go
@@ -20,7 +20,6 @@ package s3
 import (
 	"context"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/elastic/cloudbeat/resources/providers/awslib"
 	"github.com/elastic/elastic-agent-libs/logp"
 )
@@ -47,7 +46,7 @@ type Logging struct {
 
 type S3 interface {
 	DescribeBuckets(ctx context.Context) ([]awslib.AwsResource, error)
-	GetBucketACL(ctx context.Context, bucketName *string, region string) ([]types.Grant, error)
+	GetBucketACL(ctx context.Context, bucketName *string, region string) (*s3.GetBucketAclOutput, error)
 	GetBucketPolicy(ctx context.Context, bucketName *string, region string) (BucketPolicy, error)
 	GetBucketLogging(ctx context.Context, bucketName *string, region string) (Logging, error)
 }
