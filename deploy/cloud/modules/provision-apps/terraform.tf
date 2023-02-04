f4771b7f2a45833fea7cbc0bdfb001792229f69a
ofiriro3 <ofiriro3@Gmail.com>
2023-02-04 11:28:16 +0200

    Upgrade all providers

--- deploy/cloud/modules/provision-apps/terraform.tf
+++ deploy/cloud/modules/provision-apps/terraform.tf
@@ -4,10 +4,10 @@ terraform {
       source = "hashicorp/helm"
       version = ">=2.8.0"
     }
-    
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.12.1"
+      version = "~> 2.17.0"
     }
   }
 
--- deploy/cloud/modules/provision-eks-cluster/eks-cluster.tf
+++ deploy/cloud/modules/provision-eks-cluster/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.26.6"
+  version = "19.6.0"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.24"
