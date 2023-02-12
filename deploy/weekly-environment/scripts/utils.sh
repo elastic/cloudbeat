e615ee6383fb7f3a05c96c261625621d6ee28fb0
ofiriro3 <ofiriro3@Gmail.com>
2023-02-12 15:03:11 +0200

    Update local variable to lower_case

--- deploy/weekly-environment/scripts/utils.sh
+++ deploy/weekly-environment/scripts/utils.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
 
-# create a new agent policy and set POLICY_ID to the new agent id
+# create a new agent policy and set policy_id to the new agent id
 create_a_new_agent_policy() {
-  local KIBANA_URL=$1
-  local KIBANA_AUTH=$2
-  local AGENT_POLICY=$3
+  local kibana_url=$1
+  local kibana_auth=$2
+  local agent_policy=$3
 
   # Install Agent policy
   installAgentResponse=$(curl -X POST \
-    --url "${KIBANA_URL}/api/fleet/agent_policies?sys_monitoring=true" \
-    -u "$KIBANA_AUTH" \
+    --url "${kibana_url}/api/fleet/agent_policies?sys_monitoring=true" \
+    -u "$kibana_auth" \
     -H 'Cache-Control: no-cache' \
     -H 'Connection: keep-alive' \
     -H 'Content-Type: application/json' \
     -H 'kbn-xsrf: true' \
-    -d "@$AGENT_POLICY")
+    -d "@$agent_policy")
 
   check_status_code_of_curl "$installAgentResponse"
 
-  POLICY_ID=$(echo "$installAgentResponse" | jq -r '.item.id')
-  echo "Creating a new agent policy has completed successfully: New policy id: $POLICY_ID"
+  policy_id=$(echo "$installAgentResponse" | jq -r '.item.id')
+  echo "Creating a new agent policy has completed successfully: New policy id: $policy_id"
 }
 
 # create a new vanilla integration and set INTEGRATION_ID to the new integration id
 create_a_new_vanilla_integration() {
-  local KIBANA_URL=$1
-  local KIBANA_AUTH=$2
-  local POLICY_ID=$3
-  local INTEGRATION_POLICY=$4
-
-  local UPDATED_POLICY="$(jq --arg POLICY_ID "$POLICY_ID" '.policy_id |= $POLICY_ID' "$INTEGRATION_POLICY")"
-  echo "New integration policy: $UPDATED_POLICY"
+  local kibana_url=$1
+  local kibana_auth=$2
+  local policy_id=$3
+  local integration_policy=$4
+  local updated_policy
+  updated_policy="$(jq --arg policy_id "$policy_id" '.policy_id |= $policy_id' "$integration_policy")"
+  echo "New integration policy: $updated_policy"
 
   PACKAGE_POLICY_RESPONSE=$(curl -X POST \
-    --url "${KIBANA_URL}/api/fleet/package_policies" \
-    -u "$KIBANA_AUTH" \
+    --url "${kibana_url}/api/fleet/package_policies" \
+    -u "$kibana_auth" \
     -H 'Cache-Control: no-cache' \
     -H 'Connection: keep-alive' \
     -H 'Content-Type: application/json' \
     -H 'kbn-xsrf: true' \
-    -d "${UPDATED_POLICY}")
+    -d "${updated_policy}")
 
   check_status_code_of_curl "$PACKAGE_POLICY_RESPONSE"
 
-  echo "Creating a new a new vanilla integration with policy id: $POLICY_ID has completed successfully.Integration policy: $UPDATED_POLICY "
+  echo "Creating a new a new vanilla integration with policy id: $policy_id has completed successfully.Integration policy: $updated_policy "
 }
 
 # create a new vanilla integration manifest file named manifest.yaml
 create_new_vanilla_integration_manifest_file() {
-  local KIBANA_URL=$1
-  local KIBANA_AUTH=$2
-  local POLICY_ID=$3
+  local kibana_url=$1
+  local kibana_auth=$2
+  local policy_id=$3
 
   ENROLMENT_TOKEN_RESPONSE=$(curl -X GET \
-    --url "${KIBANA_URL}/api/fleet/enrollment_api_keys" \
-    -u "$KIBANA_AUTH" \
+    --url "${kibana_url}/api/fleet/enrollment_api_keys" \
+    -u "$kibana_auth" \
     -H 'Cache-Control: no-cache' \
     -H 'Connection: keep-alive' \
     -H 'Content-Type: application/json' \
@@ -62,12 +62,12 @@ create_new_vanilla_integration_manifest_file() {
 
   check_status_code_of_curl "$ENROLMENT_TOKEN_RESPONSE"
 
-  ENROLMENT_TOKEN=$(echo "$ENROLMENT_TOKEN_RESPONSE" | jq --arg policy "$POLICY_ID" '.list[] | select(.policy_id == $policy)' | jq -r '.api_key')
+  ENROLMENT_TOKEN=$(echo "$ENROLMENT_TOKEN_RESPONSE" | jq --arg policy "$policy_id" '.list[] | select(.policy_id == $policy)' | jq -r '.api_key')
   echo "ENROLMENT_TOKEN: $ENROLMENT_TOKEN"
 
   FLEET_DATA_RESPONSE=$(curl -X GET \
-    --url "${KIBANA_URL}/api/fleet/settings" \
-    -u "$KIBANA_AUTH" \
+    --url "${kibana_url}/api/fleet/settings" \
+    -u "$kibana_auth" \
     -H 'Cache-Control: no-cache' \
     -H 'Connection: keep-alive' \
     -H 'Content-Type: application/json' \
@@ -79,8 +79,8 @@ create_new_vanilla_integration_manifest_file() {
 
   # Create the manifest file
   MANIFEST_CREATION_RESPONSE=$(curl -X GET \
-    --url "${KIBANA_URL}/api/fleet/kubernetes?fleetServer=${FLEET_SERVER_HOST}&enrolToken=${ENROLMENT_TOKEN}" \
-    -u "$KIBANA_AUTH" \
+    --url "${kibana_url}/api/fleet/kubernetes?fleetServer=${FLEET_SERVER_HOST}&enrolToken=${ENROLMENT_TOKEN}" \
+    -u "$kibana_auth" \
     -H 'Cache-Control: no-cache' \
     -H 'Connection: keep-alive' \
     -H 'Content-Type: application/json' \
