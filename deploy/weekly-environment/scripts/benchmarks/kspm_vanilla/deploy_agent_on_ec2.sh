5a78456294203fa8dd5bf31a070e2f42d3a18f4d
ofiriro3 <ofiriro3@Gmail.com>
2023-02-12 15:42:22 +0200

    Updating to full path

---
+++ deploy/weekly-environment/scripts/benchmarks/kspm_vanilla/deploy_agent_on_ec2.sh
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#"cloud-init status --wait",
+#      "git clone https://github.com/elastic/cloudbeat",
+#      "cd cloudbeat",
+#      "sudo kind create cluster --config deploy/k8s/kind/kind-multi.yml --wait 30s",
+#      "sudo kind export kubeconfig --name kind-multi --kubeconfig /home/ubuntu/.kube/config",
+#      "kubectl apply -f /tmp/manifests.yml",
