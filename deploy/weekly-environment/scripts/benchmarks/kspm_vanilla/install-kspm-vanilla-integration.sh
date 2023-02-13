fe6627834e6bc2157a3a6c13fb340b0114f3a965
ofiriro3 <ofiriro3@Gmail.com>
2023-02-13 16:59:59 +0200

    Adding input validation and using 8.6.1 version

--- deploy/weekly-environment/scripts/benchmarks/kspm_vanilla/install-kspm-vanilla-integration.sh
+++ deploy/weekly-environment/scripts/benchmarks/kspm_vanilla/install-kspm-vanilla-integration.sh
@@ -8,6 +8,12 @@ KIBANA_AUTH=elastic:${KIBANA_PASSWORD}
 AGENT_POLICY=data/agent_policy_vanilla.json
 INTEGRATION_POLICY=data/package_policy_vanilla.json
 
+# Check if input is empty
+if [ -z "$KIBANA_URL" ] || [ -z "$KIBANA_PASSWORD" ]; then
+  echo "Kibana URL or Kibana password is empty"
+  exit 1
+fi
+
 ## Create a new agent policy And get the agent id
 create_a_new_agent_policy "$KIBANA_URL" "$KIBANA_AUTH" "$AGENT_POLICY"
 
