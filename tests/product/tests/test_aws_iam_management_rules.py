796a295a4027ab8f2993a0061d4dce496148953a
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2023-03-14 23:05:39 +0200

    implement tests for aws iam management rules (#796)

--- tests/product/tests/test_aws_iam_management_rules.py
+++ tests/product/tests/test_aws_iam_management_rules.py
@@ -1,5 +1,5 @@
 """
-CIS AWS S3 Service rules verification.
+CIS AWS IAM management rules verification.
 This module verifies correctness of retrieved findings by manipulating audit actions
 """
 from datetime import datetime, timedelta
@@ -7,12 +7,12 @@ from functools import partial
 import pytest
 from commonlib.utils import get_ES_evaluation, identifier_by_name
 
-from product.tests.data.aws import aws_s3_test_cases as aws_s3_tc
+from product.tests.data.aws import aws_iam_test_cases as aws_iam_tc
 from product.tests.parameters import register_params, Parameters
 
 
-@pytest.mark.aws_s3_service_rules
-def test_aws_s3_service_rules(
+@pytest.mark.aws_iam_rules
+def test_aws_iam_management_rules(
     elastic_client,
     cloudbeat_agent,
     rule_tag,
@@ -29,14 +29,14 @@ def test_aws_s3_service_rules(
     @param expected: Result to be found in finding evaluation field.
     @return: None - Test Pass / Fail result is generated.
     """
-    s3_identifier = partial(identifier_by_name, case_identifier)
+    iam_identifier = partial(identifier_by_name, case_identifier)
 
     evaluation = get_ES_evaluation(
         elastic_client=elastic_client,
         timeout=cloudbeat_agent.aws_findings_timeout,
         rule_tag=rule_tag,
-        exec_timestamp=datetime.utcnow() - timedelta(hours=2),
-        resource_identifier=s3_identifier,
+        exec_timestamp=datetime.utcnow() - timedelta(minutes=30),
+        resource_identifier=iam_identifier,
     )
 
     assert evaluation is not None, f"No evaluation for rule {rule_tag} could be found"
@@ -44,10 +44,10 @@ def test_aws_s3_service_rules(
 
 
 register_params(
-    test_aws_s3_service_rules,
+    test_aws_iam_management_rules,
     Parameters(
         ("rule_tag", "case_identifier", "expected"),
-        [*aws_s3_tc.cis_aws_s3_cases.values()],
-        ids=[*aws_s3_tc.cis_aws_s3_cases.keys()],
+        [*aws_iam_tc.cis_aws_iam_cases.values()],
+        ids=[*aws_iam_tc.cis_aws_iam_cases.keys()],
     ),
 )
