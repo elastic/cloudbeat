e6ce4d86ff2e0fcc3b9fe1c108186493378ba334
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2023-03-22 14:51:08 +0200

    add skip for aws logging tests (including bug links)

--- tests/product/tests/data/aws/aws_logging_test_cases.py
+++ tests/product/tests/data/aws/aws_logging_test_cases.py
@@ -124,8 +124,11 @@ cis_aws_log_3_4_fail = EksAwsServiceCase(
 )
 
 cis_aws_log_3_4 = {
-    "3.4 Ensure CloudTrail integration with CloudWatch, integration exists expected passed": cis_aws_log_3_4_pass,
     "3.4 Ensure CloudTrail integration with CloudWatch, no integration expected failed": cis_aws_log_3_4_fail,
+}
+
+cis_aws_log_3_4_skip = {
+    "3.4 Ensure CloudTrail integration with CloudWatch, integration exists expected passed": cis_aws_log_3_4_pass,
 }
 
 cis_aws_log_3_6_pass = EksAwsServiceCase(
@@ -141,8 +144,11 @@ cis_aws_log_3_6_fail = EksAwsServiceCase(
 )
 
 cis_aws_log_3_6 = {
-    "3.6 Ensure CloudTrail access logging, enabled=true expected passed": cis_aws_log_3_6_pass,
     "3.6 Ensure CloudTrail access logging, enabled=false expected failed": cis_aws_log_3_6_fail,
+}
+
+cis_aws_log_3_6_skip = {
+    "3.6 Ensure CloudTrail access logging, enabled=true expected passed": cis_aws_log_3_6_pass,
 }
 
 cis_aws_log_3_7_pass = EksAwsServiceCase(
@@ -224,6 +230,9 @@ cis_aws_log_3_11_fail = EksAwsServiceCase(
 
 cis_aws_log_3_11 = {
     "3.11 Ensure Object-level logging read events, enabled=false, expected failed ": cis_aws_log_3_11_fail,
+}
+
+cis_aws_log_3_11_skip = {
     "3.11 Ensure Object-level logging read events, enabled=true, expected passed": cis_aws_log_3_11_pass,
 }
 
@@ -240,17 +249,45 @@ cis_aws_log_cases = {
     **cis_aws_log_3_2,
     **cis_aws_log_3_3,
     **cis_aws_log_3_4,
+    **skip_param_case(
+        cis_aws_log_3_4_skip,
+        data_to_report=SkipReportData(
+            skip_reason=(
+                "When CloudTrail integration with CloudWatch is enabled, Cloudbeat returns evaluation result fail"
+            ),
+            url_title="cloudbeat: #809",
+            url_link="https://github.com/elastic/cloudbeat/issues/809",
+        ),
+    ),
     **cis_aws_log_3_6,
+    **skip_param_case(
+        cis_aws_log_3_6_skip,
+        data_to_report=SkipReportData(
+            skip_reason=(
+                "When S3 bucket access logging is enabled on the CloudTrail S3 bucket evaluation result is failed"
+            ),
+            url_title="cloudbeat: #810",
+            url_link="https://github.com/elastic/cloudbeat/issues/810",
+        ),
+    ),
     **cis_aws_log_3_7,
     **cis_aws_log_3_9,
     **cis_aws_log_3_10,
     **skip_param_case(
         cis_aws_log_3_10_skip,
         data_to_report=SkipReportData(
-            skip_reason="Test case data generation issue",
-            url_title="security-team: #6204",
-            url_link="https://github.com/elastic/security-team/issues/6204",
+            skip_reason="When object-level logging for write/read events is enabled for S3 bucket evaluation is failed",
+            url_title="cloudbeat: #811",
+            url_link="https://github.com/elastic/cloudbeat/issues/811",
         ),
     ),
     **cis_aws_log_3_11,
+    **skip_param_case(
+        cis_aws_log_3_11_skip,
+        data_to_report=SkipReportData(
+            skip_reason="When object-level logging for write/read events is enabled for S3 bucket evaluation is failed",
+            url_title="cloudbeat: #811",
+            url_link="https://github.com/elastic/cloudbeat/issues/811",
+        ),
+    ),
 }
