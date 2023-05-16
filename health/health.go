4185dac9e28c5d0d7da7f1f17dcb2d18aecc2ce4
Amir Ben Nun <amir.benun7@gmail.com>
2023-05-16 13:10:38 +0300

    mutex

--- health/health.go
+++ health/health.go
@@ -2,19 +2,24 @@ package health
 
 import (
 	"errors"
+	"sync"
 )
 
 var Reporter = &reporter{
 	ch:     make(chan error, 1),
-	errors: make(map[string]error),
+	errors: map[string]error{},
+	mut:    sync.Mutex{},
 }
 
 type reporter struct {
 	ch     chan error
 	errors map[string]error
+	mut    sync.Mutex
 }
 
 func (r *reporter) NewHealth(component string, err error) {
+	r.mut.Lock()
+	defer r.mut.Unlock()
 	r.errors[component] = err
 	list := make([]error, 0, len(r.errors))
 	for _, err := range r.errors {
