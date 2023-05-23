45badd992555c5761336aa7e1c55f2d94357b16a
Uri Weisman <68195305+uri-weisman@users.noreply.github.com>
2023-05-23 11:55:11 +0300

    Buildkite migration (#944)

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-# linux/amd64 is in the default list already, set here
-# to prevent jenkins_release.sh from adding more PLATFORMS
 export PLATFORMS="linux/amd64,linux/arm64"
 export TYPES="tar.gz"
 
-make activate-hermit
-
+source ./bin/activate-hermit
 if [ $WORKFLOW = "staging" ] ; then
     make release-manager-release
 else
