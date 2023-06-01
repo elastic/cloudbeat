dd21e8767df4067144a653ab1e8476885dbba565
uri-weisman <uri.weisman@elastic.co>
2023-06-01 16:39:08 +0300

    working as expected

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -7,7 +7,6 @@ CLOUDBEAT_VERSION=$(grep defaultBeatVersion version/version.go | cut -d'=' -f2 |
 PYTHON_BIN=./build/ve/$(go env GOOS)/bin
 PYTHON=$PYTHON_BIN/python
 
-
 if [ "$WORKFLOW" = "snapshot" ] ; then
     export SNAPSHOT="true"
 fi
@@ -18,7 +17,7 @@ mage pythonEnv
 mage package
 
 CSV_FILE="build/dependencies-${CLOUDBEAT_VERSION}"
-[ -n "$SNAPSHOT" ] && CSV_FILE+="-SNAPSHOT"
+[ -n "${SNAPSHOT+x}" ] && CSV_FILE+="-SNAPSHOT"
 
 echo "Generating $CSV_FILE.csv"
 $PYTHON ./.buildkite/scripts/generate_notice.py --csv "$CSV_FILE.csv"
