33ab20bd05522c76c49c742860f87b0be6471f58
Uri Weisman <68195305+uri-weisman@users.noreply.github.com>
2023-06-01 12:17:44 +0300

    Remove makefile (#1020)

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-set -euox pipefail
+set -uox pipefail
 
 export PLATFORMS="linux/amd64,linux/arm64"
 export TYPES="tar.gz"
+CLOUDBEAT_VERSION=$(grep defaultBeatVersion version/version.go | cut -d'=' -f2 | tr -d '" ')
+
+if [ "$WORKFLOW" = "snapshot" ] ; then
+    export SNAPSHOT="true"
+fi
 
 source ./bin/activate-hermit
-if [ $WORKFLOW = "staging" ] ; then
-    make release-manager-release
-else
-    export SNAPSHOT="true"
-    make release-manager-snapshot
-fi
+
+mage pythonEnv
+
+./bin/python3 ./.buildkite/scripts/generate_notice.py --csv build/dependencies-"${CLOUDBEAT_VERSION}"-"${WORKFLOW}".csv
+
+mage package
 
 cp build/dependencies-*.csv build/distributions/.
