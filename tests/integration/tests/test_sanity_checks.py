c22a2b926a0978f943de53fdab969a560a0e8f36
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2023-06-20 21:17:40 +0300

    update findings timeout

--- tests/integration/tests/test_sanity_checks.py
+++ tests/integration/tests/test_sanity_checks.py
@@ -9,7 +9,7 @@ verifying that there are findings of 'resource.type' for each feature.
 import pytest
 from commonlib.utils import get_findings
 
-CONFIG_TIMEOUT = 60
+CONFIG_TIMEOUT = 300
 
 tests_data = {
     "cis_aws": [
