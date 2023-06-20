elastic/cloudbeat

commit 98c0bc5b35b64d95f6c1014c6f5869451496b0eb

Move docker files to deploy folder (#1043)

justfile
@@ -95,7 +95,7 @@ deploy-cloudbeat-nocert:
 # Builds cloudbeat docker image with the OPA bundle included and the debug flag
 build-cloudbeat-debug $GOARCH=LOCAL_GOARCH: build-opa-bundle
   GOOS=linux go mod vendor
-  GOOS=linux CGO_ENABLED=0 go build -gcflags "all=-N -l" && docker build -f Dockerfile.debug -t cloudbeat . --platform=linux/$GOARCH
+  GOOS=linux CGO_ENABLED=0 go build -gcflags "all=-N -l" && docker build -f deploy/Dockerfile.debug -t cloudbeat . --platform=linux/$GOARCH
 
 delete-cloudbeat:
   cp {{env_var('ELASTIC_PACKAGE_CA_CERT')}} {{kustomizeVanillaOverlay}}

.github/workflows/cloudbeat-ci.yml
@@ -91,6 +91,7 @@ jobs:
         uses: docker/build-push-action@v4
         with:
           context: .
+          file: ./deploy/Dockerfile
           push: false
           tags: cloudbeat:latest
           cache-from: type=local,src=/tmp/.buildx-cache

.github/workflows/eks-ci.yml
@@ -92,6 +92,7 @@ jobs:
         uses: docker/build-push-action@v4
         with:
           context: .
+          file: ./deploy/Dockerfile
           push: false
           tags: cloudbeat:latest
           cache-from: type=local,src=/tmp/.buildx-cache

.github/workflows/k8s-ci.yml
@@ -62,6 +62,7 @@ jobs:
         uses: docker/build-push-action@v4
         with:
           context: .
+          file: ./deploy/Dockerfile
           push: false
           tags: cloudbeat:latest
           cache-from: type=local,src=/tmp/.buildx-cache

.github/workflows/periodic-ci.yml
@@ -104,6 +104,7 @@ jobs:
         uses: docker/build-push-action@v4
         with:
           context: .
+          file: ./deploy/Dockerfile
           push: false
           tags: cloudbeat:latest
           cache-from: type=local,src=/tmp/.buildx-cache

commit 47e19c97fc5271509b5ceca0925cb5eb34491805

Fix justfile build-cloudbeat
Broken after https://github.com/elastic/cloudbeat/pull/1043

justfile
@@ -79,7 +79,7 @@ alias build-cloudbeat := build-cloudbeat-docker-image
 build-cloudbeat-docker-image $GOARCH=LOCAL_GOARCH: build-opa-bundle
   just build-binary $GOARCH
   @echo "Building cloudbeat docker image for linux/$GOARCH"
-  docker build -t cloudbeat . --platform=linux/$GOARCH
+  docker build -f deploy/Dockerfile -t cloudbeat . --platform=linux/$GOARCH
 
 deploy-cloudbeat:
   cp {{env_var('ELASTIC_PACKAGE_CA_CERT')}} {{kustomizeVanillaOverlay}}