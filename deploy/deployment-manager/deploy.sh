35ab4f6b46e4d37befb2f8ba68059fa7cd0ab381
uri-weisman <uri.weisman@elastic.co>
2023-08-02 14:53:53 +0300

    roolback on deployment failure and exit script in case a single command failed

--- deploy/deployment-manager/deploy.sh
+++ deploy/deployment-manager/deploy.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+# Function to run a gcloud command and check its exit code
+run_command() {
+    eval $1
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "Error: Command \"$1\" failed with exit code $status. Exiting..."
+        exit $status
+    fi
+}
+
 # Set environment variables with the name and number of your project.
 export PROJECT_NAME=$(gcloud config get-value core/project)
 export PROJECT_NUMBER=$(gcloud projects list --filter=${PROJECT_NAME} --format="value(PROJECT_NUMBER)")
 
 # Enable the Google Cloud APIs needed for misconfiguration scanning
-gcloud services enable iam.googleapis.com deploymentmanager.googleapis.com compute.googleapis.com cloudresourcemanager.googleapis.com cloudasset.googleapis.com
+run_command "gcloud services enable iam.googleapis.com deploymentmanager.googleapis.com compute.googleapis.com cloudresourcemanager.googleapis.com cloudasset.googleapis.com"
 
 # Add the needed roles to apply the templates to the project using the deployment manager
-gcloud projects add-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/iam.roleAdmin
-gcloud projects add-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/resourcemanager.projectIamAdmin
+run_command "gcloud projects add-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/iam.roleAdmin"
+run_command "gcloud projects add-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/resourcemanager.projectIamAdmin"
 
 # Apply the deployment manager templates
-gcloud deployment-manager deployments create elastic-agent-cspm --project ${PROJECT_NAME} \
-    --template deploy/deployment-manager/compute_engine.py --properties elasticAgentVersion:${STACK_VERSION},fleetUrl:${FLEET_URL},enrollmentToken:${ENROLLMENT_TOKEN}
+run_command "gcloud deployment-manager deployments create --automatic-rollback-on-error elastic-agent-cspm --project ${PROJECT_NAME} \
+    --template deploy/deployment-manager/compute_engine.py --properties elasticAgentVersion:${STACK_VERSION},fleetUrl:${FLEET_URL},enrollmentToken:${ENROLLMENT_TOKEN}"
 
 # Remove the roles required to deploy the DM templates
-gcloud projects remove-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/iam.roleAdmin
-gcloud projects remove-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/resourcemanager.projectIamAdmin
+run_command "gcloud projects remove-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/iam.roleAdmin"
+run_command "gcloud projects remove-iam-policy-binding ${PROJECT_NAME} --member=serviceAccount:${PROJECT_NUMBER}@cloudservices.gserviceaccount.com --role=roles/resourcemanager.projectIamAdmin"
