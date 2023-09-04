979271fd15f1da51809c5fa9be105493e051a74e
Amir Ben Nun <amir.benun7@gmail.com>
2023-09-04 12:14:37 +0300

    test k8s getid uuid

--- dataprovider/providers/k8s/id_provider_test.go
+++ dataprovider/providers/k8s/id_provider_test.go
@@ -26,7 +26,7 @@ import (
 	fetchers "github.com/elastic/cloudbeat/resources/fetching/fetchers/k8s"
 )
 
-func Test_k8sIdProvider_GetId(t *testing.T) {
+func Test_k8sIdProvider_GetIdInCluster(t *testing.T) {
 	tests := []struct {
 		name     string
 		want     string
@@ -56,6 +56,41 @@ func Test_k8sIdProvider_GetId(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			p := NewIdProvider("cluster_id", "node_id")
 			data := p.getIdInCluster(tt.resource, tt.id)
+			assert.Equal(t, tt.want, data)
+		})
+	}
+}
+
+func Test_k8sIdProvider_GetId(t *testing.T) {
+	tests := []struct {
+		name     string
+		want     string
+		resource string
+		id       string
+	}{
+		{
+			name: "unknown resource should return the raw id",
+			id:   "metadata_id",
+			want: "16d3e3ff-583a-5b0d-86e2-1f7c6675dfb3",
+		},
+		{
+			name:     "CAAS resource should return cluster id",
+			id:       "metadata_id",
+			want:     "e0b7df83-548e-5f75-b460-23bf3a96f679",
+			resource: fetching.CloudContainerMgmt,
+		},
+		{
+			name:     "process reource should add cluster id and node id",
+			id:       "metadata_id",
+			want:     "76389fcc-375d-5acc-8e0a-db814362fc50",
+			resource: fetchers.ProcessResourceType,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := NewIdProvider("cluster_id", "node_id")
+			data := p.GetId(tt.resource, tt.id)
 			assert.Equal(t, tt.want, data)
 		})
 	}
