f1e331ad21abc9ea5de82167cbb7deb478d006c1
Amir Ben Nun <amir.benun7@gmail.com>
2023-09-18 15:00:19 +0300

    builder tests

---
+++ flavors/benchmark/builder/builder_test.go
@@ -0,0 +1,57 @@
+package builder
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/elastic/cloudbeat/config"
+	"github.com/elastic/cloudbeat/dataprovider"
+	"github.com/elastic/cloudbeat/resources/utils/testhelper"
+	"github.com/elastic/cloudbeat/uniqueness"
+)
+
+func TestBase_Success(t *testing.T) {
+	tests := []struct {
+		name      string
+		opts      []Option
+		benchType interface{}
+	}{
+		{
+			name:      "by default create base benchmark",
+			benchType: &basebenchmark{},
+		},
+		{
+			name: "with opts create base benchmark",
+			opts: []Option{
+				WithIdProvider(dataprovider.NewMockIdProvider(t)),
+				WithManagerTimeout(0),
+				WithBenchmarkDataProvider(dataprovider.NewMockCommonDataProvider(t)),
+			},
+			benchType: &basebenchmark{},
+		},
+		{
+			name: "with leader elector create k8s benchmark",
+			opts: []Option{
+				WithK8sLeaderElector(uniqueness.NewMockManager(t)),
+			},
+			benchType: &k8sbenchmark{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			log := testhelper.NewLogger(t)
+			path, err := filepath.Abs("../../../bundle.tar.gz")
+			assert.NoError(t, err)
+
+			benchmark, err := New(tt.opts...).Build(context.Background(), log, &config.Config{
+				BundlePath: path,
+			}, nil, nil)
+			assert.NoError(t, err)
+			assert.IsType(t, tt.benchType, benchmark)
+		})
+	}
+}
