c91a506f4ac8d2c3ebb8f52e1688ecc6e9ffa211
Orestis Floros <orestis.floros@elastic.co>
2023-09-28 09:46:05 +0200

    wait && ignore errors

--- deploy/azure/install-agent.sh
+++ deploy/azure/install-agent.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 # Delete password, stop and disable ssh
 sudo passwd --delete cloudbeat &
 (
-    sudo systemctl disable --now ssh
-    sudo systemctl mask ssh.service
-    sudo killall sshd
+    sudo systemctl disable --now ssh || true
+    sudo systemctl mask ssh.service || true
+    sudo killall sshd || true
 ) &
 
 usage="$0 <elastic agent version> <elastic artifact server> <fleet url> <enrollment token>"
@@ -20,3 +20,5 @@ curl -L -O "${ElasticArtifactServer}/$ElasticAgentArtifact.tar.gz"
 tar xzf "${ElasticAgentArtifact}.tar.gz"
 cd "${ElasticAgentArtifact}"
 sudo ./elastic-agent install --non-interactive --url="${FleetUrl}" --enrollment-token="${EnrollmentToken}"
+
+wait
