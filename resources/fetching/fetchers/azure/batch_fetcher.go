eadc8bf174f4cc5bdbef10809805982b5e9ca15a
Orestis Floros <orestis.floros@elastic.co>
2023-10-05 17:28:55 +0200

    Azure: Batch Fetcher: Group resource by subscription ID
    Fixes #1391

--- resources/fetching/fetchers/azure/batch_fetcher.go
+++ resources/fetching/fetchers/azure/batch_fetcher.go
@@ -22,6 +22,8 @@ import (
 	"fmt"
 
 	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/samber/lo"
+	"golang.org/x/exp/maps"
 
 	"github.com/elastic/cloudbeat/resources/fetching"
 	"github.com/elastic/cloudbeat/resources/providers/azurelib"
@@ -50,21 +52,26 @@ func NewAzureBatchAssetFetcher(log *logp.Logger, ch chan fetching.ResourceInfo, 
 func (f *AzureBatchAssetFetcher) Fetch(ctx context.Context, cMetadata fetching.CycleMetadata) error {
 	f.log.Info("Starting AzureBatchAssetFetcher.Fetch")
 
-	for assetType, pair := range AzureBatchAssets {
-		assets, err := f.provider.ListAllAssetTypesByName([]string{assetType})
-		if err != nil {
-			return err
-		}
+	allAssets, err := f.provider.ListAllAssetTypesByName(maps.Keys(AzureBatchAssets))
+	if err != nil {
+		return err
+	}
 
-		if len(assets) == 0 {
-			continue
-		}
+	if len(allAssets) == 0 {
+		return nil
+	}
+
+	assetGroups := lo.GroupBy(allAssets, func(item inventory.AzureAsset) string {
+		return fmt.Sprintf("%s-%s", item.SubscriptionId, item.Type)
+	})
+
+	for _, assets := range assetGroups {
+		pair := AzureBatchAssets[assets[0].Type]
 
 		select {
 		case <-ctx.Done():
 			f.log.Infof("AzureBatchAssetFetcher.Fetch context err: %s", ctx.Err().Error())
 			return nil
-		// TODO: Groups by subscription id to create multiple batches of assets
 		case f.resourceCh <- fetching.ResourceInfo{
 			CycleMetadata: cMetadata,
 			Resource: &AzureBatchResource{
