9cb81d6cb109d225c45999d99233f54fe7fe58c5
Orestis Floros <orestis.floros@elastic.co>
2023-11-07 11:01:37 +0100

    comment

--- .ci/scripts/update-beats.sh
+++ .ci/scripts/update-beats.sh
@@ -4,4 +4,10 @@ set -euxo pipefail
 BEATS_VERSION=${1:?Missing version argument}
 go get "github.com/elastic/beats/v7@$BEATS_VERSION"
 go mod tidy
+# updatecli needs some specific stdout when using the shell target,
+# see https://www.updatecli.io/docs/plugins/resource/shell/#_shell_target:
+# > When the commands runs successfully (e.g. with an exit code of zero), the behavior depends on the content of the
+# standard output:
+# > - If it is empty, then updatecli report a success with no changes applied.
+# > - Otherwise updatecli report a success with the content of the standard output as the resulting value of the change.
 git diff
