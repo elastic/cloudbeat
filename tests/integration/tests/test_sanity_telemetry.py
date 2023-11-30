1889ba1e35058d51c6f0ff1c28d805e015831e89
Ido Cohen <90558359+CohenIdo@users.noreply.github.com>
2023-11-30 09:49:34 +0200

    first code review comments

--- tests/integration/tests/test_sanity_telemetry.py
+++ tests/integration/tests/test_sanity_telemetry.py
@@ -6,15 +6,14 @@ have been created.
 The goal of this suite is to perform basic sanity checks to verify the telemetry fetchers are working
  as expected.
 """
-# pylint: disable=redefined-outer-name
 import pytest
 from commonlib.utils import get_telemetry
 from configuration import elasticsearch
 from munch import munchify
 
 
-@pytest.fixture(scope="module")
-def cloud_security_telemetry_data():
+@pytest.fixture(scope="module", name="cloud_security_telemetry_data")
+def get_cloud_security_telemetry_data():
     """Fixture to fetch telemetry data"""
     telemetry_payload = get_telemetry(elasticsearch)
     telemetry_object = munchify(telemetry_payload[0])
@@ -54,30 +53,23 @@ def test_telemetry_cloud_account_stats(cloud_security_telemetry_data):
     # account stats
     cloud_account_stats = cloud_security_telemetry_data.cloud_account_stats
     for account in cloud_account_stats:
+        assert len(account.account_id) > 0, f"Telemetry data missing account_id for cloud_account_stats {account}"
+        assert len(account.product) > 0, f"Telemetry data missing product for cloud_account_stats {account}"
         assert (
-            len(account.account_id) > 0
-        ), f"Telemetry data missing account_id for \
-            cloud_account_stats {account}"
-
-        assert (
-            len(account.product) > 0
-        ), f"Telemetry data missing product for \
-             cloud_account_stats {account}"
+            len(account.package_policy_id) > 0
+        ), f"Telemetry data missing package_policy_id for cloud_account_stats {account}"
 
         if not (account.product == "kspm" and "CIS Kubernetes" in account.posture_management_stats.benchmark_name):
             assert (
                 len(account.cloud_provider) > 0
-            ), f"Telemetry data missing cloud_provider for \
-                    cloud_account_stats {account}"
+            ), f"Telemetry data missing cloud_provider for cloud_account_stats {account}"
         if account.product != "vuln_mgmt":
             assert (
                 len(account.posture_management_stats.benchmark_name) > 0
-            ), f"Telemetry data missing benchmark_name \
-                 for cloud_account_stats {account}"
+            ), f"Telemetry data missing benchmark_name for cloud_account_stats {account}"
             assert (
                 len(account.posture_management_stats.benchmark_version) > 0
-            ), f"Telemetry data missing \
-                benchmark_version for cloud_account_stats {account}"
+            ), f"Telemetry data missing benchmark_version for cloud_account_stats {account}"
 
 
 @pytest.mark.sanity
@@ -91,33 +83,19 @@ def test_telemetry_installation_stats(cloud_security_telemetry_data):
     # installation stats
     installation_stats = cloud_security_telemetry_data.installation_stats
     for installation in installation_stats:
-        assert (
-            len(installation.package_policy_id) > 0
-        ), "Telemetry data missing package_policy_id in \
-                installation stats"
-        assert (
-            len(installation.feature) > 0
-        ), f"Telemetry data missing feature in installation_stats \
-                for {installation}"
-        assert (
-            installation.agent_count > 0
-        ), f"Telemetry data missing agent in installation_stats \
-                for {installation}"
+        assert len(installation.package_policy_id) > 0, "Telemetry data missing package_policy_id in installation stats"
+        assert len(installation.feature) > 0, f"Telemetry data missing feature in installation_stats for {installation}"
+        assert installation.agent_count > 0, f"Telemetry data missing agent in installation_stats for {installation}"
         assert (
             len(installation.package_version) > 0
-        ), f"Telemetry data missing package_version in installation_stats \
-                for {installation}"
+        ), f"Telemetry data missing package_version in installation_stats for {installation}"
         assert (
             len(installation.deployment_mode) > 0
-        ), f"Telemetry data missing deployment_mode in installation_stats \
-                            for {installation}"
+        ), f"Telemetry data missing deployment_mode in installation_stats for {installation}"
         if installation.feature == "cspm":
             assert isinstance(
-                installation.is_setup_automatic,
-                bool,
-            ), f"Telemetry data missing is_setup_automatic \
-                 in installation_stats for {installation}"
+                installation.is_setup_automatic, bool
+            ), f"Telemetry data missing is_setup_automatic in installation_stats for {installation}"
             assert (
                 len(installation.account_type) > 0
-            ), f"Telemetry data missing account_type in installation_stats \
-                                    for {installation}"
+            ), f"Telemetry data missing account_type in installation_stats for {installation}"
