d3b8697ff492ad19f5337956d8cff362a571fd28
Ido Cohen <90558359+CohenIdo@users.noreply.github.com>
2023-11-30 16:12:49 +0200

    Add telemetry automation test

--- deploy/test-environments/fleet_api/src/configuration_fleet.py
+++ deploy/test-environments/fleet_api/src/configuration_fleet.py
@@ -31,7 +31,7 @@ CNVM_TAGS = (
 elk_config = Munch()
 elk_config.user = os.getenv("ES_USER", "NA")
 elk_config.password = os.getenv("ES_PASSWORD", "NA")
-elk_config.kibana_url = os.getenv("KIBANA_URL", "NA")
+elk_config.kibana_url = os.getenv("KIBANA_URL", "")
 elk_config.stack_version = os.getenv("STACK_VERSION", "NA")
 elk_config.auth = (elk_config.user, elk_config.password)
 
