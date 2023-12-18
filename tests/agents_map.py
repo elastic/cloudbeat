4b80749550c6b45d74f053c3c7a8f279c0e806f0
Amir Ben Nun <amir.benun7@gmail.com>
2023-12-18 12:00:22 +0200

    lint

--- tests/agents_map.py
+++ tests/agents_map.py
@@ -8,7 +8,7 @@ from munch import Munch
 from configuration import elasticsearch
 
 sys.path.append(os.path.relpath("../deploy/test-environments/fleet_api/src"))
-from api.agent_policy_api import get_agents # pylint: disable=wrong-import-position
+from api.agent_policy_api import get_agents  # pylint: disable=wrong-import-position
 
 CIS_AWS_COMPONENT = "cloudbeat/cis_aws"
 CIS_GCP_COMPONENT = "cloudbeat/cis_gcp"
