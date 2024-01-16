510e20e4b2804169c9f826c9735d0f2a98f7fd45
Or Ouziel <or.ouziel@elastic.co>
2024-01-16 17:05:23 +0200

    bump azure cycle duration to 20m

--- flavors/benchmark/azure.go
+++ flavors/benchmark/azure.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/elastic/elastic-agent-libs/logp"
 
@@ -49,6 +50,7 @@ func (a *Azure) NewBenchmark(ctx context.Context, log *logp.Logger, cfg *config.
 
 	return builder.New(
 		builder.WithBenchmarkDataProvider(bdp),
+		builder.WithManagerTimeout(20 * time.Minute),
 	).Build(ctx, log, cfg, resourceCh, reg)
 }
 
