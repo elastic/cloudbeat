51dfbcd30eb77818e759cbe4e8858048741eafe0
Evgeniy Belyi <jeniawhite92@gmail.com>
2024-01-22 16:23:33 +0200

    uncomment

--- tests/product/tests/data/azure/azure_virtual_machine_test_cases.py
+++ tests/product/tests/data/azure/azure_virtual_machine_test_cases.py
@@ -8,7 +8,6 @@ from ..azure_test_case import AzureServiceCase
 from ..constants import RULE_PASS_STATUS, RULE_FAIL_STATUS
 
 CIS_7_1 = "CIS 7.1"
-# Fail not working
 CIS_7_2 = "CIS 7.2"
 # No evaluation
 CIS_7_3 = "CIS 7.3"
