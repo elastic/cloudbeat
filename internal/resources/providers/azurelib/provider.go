92442f2f12caf17df575e7e8a69677c099a6a88b
kubasobon <wtty.fool@gmail.com>
2024-01-23 11:33:40 +0100

    update all Provider structs

--- internal/resources/providers/azurelib/provider.go
+++ internal/resources/providers/azurelib/provider.go
@@ -71,6 +71,7 @@ func (p *ProviderInitializer) Init(log *logp.Logger, azureConfig auth.AzureFacto
 
 	resourceGraphProvider := inventory.NewResourceGraphProvider(log, resourceGraphClientFactory)
 	return &provider{
+		AppServiceProviderAPI:       inventory.NewAppServiceProvider(log, azureConfig.Credentials),
 		KeyVaultProviderAPI:         inventory.NewKeyVaultProvider(log, azureConfig.Credentials),
 		MysqlProviderAPI:            inventory.NewMysqlProvider(log, azureConfig.Credentials),
 		PostgresqlProviderAPI:       inventory.NewPostgresqlProvider(log, azureConfig.Credentials),
@@ -85,6 +86,7 @@ func (p *ProviderInitializer) Init(log *logp.Logger, azureConfig auth.AzureFacto
 
 type provider struct {
 	governance.ProviderAPI
+	inventory.AppServiceProviderAPI
 	inventory.KeyVaultProviderAPI
 	inventory.MysqlProviderAPI
 	inventory.PostgresqlProviderAPI
