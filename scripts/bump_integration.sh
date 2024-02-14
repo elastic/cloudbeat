65193c8f8da28f536091163894235e6a17000eab
Or Ouziel <or.ouziel@elastic.co>
2024-02-14 21:45:21 +0200

    use preview version for integration

--- scripts/bump_integration.sh
+++ scripts/bump_integration.sh
@@ -17,18 +17,12 @@ checkout_integration_repo() {
     git checkout -b "$BRANCH" origin/main
 }
 
-# reads the last version from changelog.yml version map
-# and increments the minor version
 get_next_integration_version() {
-    echo "• Get next integration version"
-    input_line=$(sed -n '3p' $CHANGELOG_PATH) # last version is always on line 3
-    first_version=$(echo "$input_line" | cut -d' ' -f2)
-    major_minor=$(echo "$first_version" | cut -d'.' -f1-2)
-    major=$(echo "$major_minor" | cut -d'.' -f1)
-    minor=$(echo "$major_minor" | cut -d'.' -f2)
-    next_minor=$((minor + 1))
-    export NEXT_INTEGRATION_VERSION="$major.$next_minor.0"
-    echo "NEXT_INTEGRATION_VERSION: $NEXT_INTEGRATION_VERSION"
+    current_version=$(yq '.[0].version' changelog.yml | tr -d '"')
+    version_number=$(echo "$current_version" | cut -d '.' -f 1-2)
+    minor_version=$(echo "$current_version" | cut -d '.' -f 3 | cut -d '-' -f 1)
+    next_minor_version=$((minor_version + 1))
+    export NEXT_INTEGRATION_VERSION="$version_number.$(printf "%02d" $next_minor_version)-preview"
 }
 
 update_manifest_version_vars() {
@@ -87,21 +81,9 @@ update_changelog_version() {
     git push origin "$BRANCH"
 }
 
-update_changelog_version_map() {
-    echo "• Update changelog version map"
-    next_minor=$(echo "$NEXT_INTEGRATION_VERSION" | cut -d'.' -f1,2)
-    new_comment="# ${next_minor}.x - ${MAJOR_MINOR_CLOUDBEAT}.x"
-    new_file_content=$(awk -v var="$new_comment" 'NR==3 {print var} {print}' "$CHANGELOG_PATH")
-    echo -e "$new_file_content" >temp.yaml
-    mv temp.yaml "$CHANGELOG_PATH"
-    git add $CHANGELOG_PATH
-    git commit -m "Update changelog version map"
-    git push origin "$BRANCH"
-}
-
 checkout_integration_repo
 get_next_integration_version
 update_manifest_version_vars
 update_manifest_version
 update_changelog_version
-update_changelog_version_map
+create_integrations_pr
