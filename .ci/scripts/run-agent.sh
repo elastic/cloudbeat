79a87a0e87e72706f688b78b5541063e94526633
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2024-04-02 18:46:28 +0300

    update workflow and script

--- .ci/scripts/run-agent.sh
+++ .ci/scripts/run-agent.sh
@@ -4,10 +4,8 @@ ELASTIC_AGENT_VERSION=$1
 
 curl -L -O "https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64.tar.gz"
 tar xzvf "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64.tar.gz"
+cp ./tests/test_environments/agents/elastic-agent-8-13.yml "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent.yml"
 cd "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64" || exit
 
-# TODO: Replace elastic-agent.yml with the correct configuration file
-# cp /path/to/elastic-agent.yml /path/to/elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent.yml
-
 # Install the Elastic Agent
-# sudo ./elastic-agent install -f
+sudo ./elastic-agent install -f
