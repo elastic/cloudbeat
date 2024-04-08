84786ac7ae61193b8ffe8f3dbfaa64f078bc0313
Kostas Stamatakis <moukoublen@gmail.com>
2024-04-08 12:40:23 +0300

    fix panic due to missing logger of aws IdentityProvider (#2095)
    fix missing logger of aws IdentityProvider
    
    (cherry picked from commit 6bf970afbda452753e3fbf671bd5bc917c0f0e60)
    
    # Conflicts:
    #	internal/flavors/asset_inventory.go
    #	internal/flavors/benchmark/strategy.go

--- internal/flavors/benchmark/strategy.go
+++ internal/flavors/benchmark/strategy.go
@@ -38,23 +38,28 @@ type Strategy interface {
 	checkDependencies() error
 }
 
-func GetStrategy(cfg *config.Config) (Strategy, error) {
+func GetStrategy(cfg *config.Config, log *logp.Logger) (Strategy, error) {
 	switch cfg.Benchmark {
 	case config.CIS_AWS:
 		if cfg.CloudConfig.Aws.AccountType == config.OrganizationAccount {
 			return &AWSOrg{
+<<<<<<< HEAD
 				IdentityProvider: awslib.IdentityProvider{},
+=======
+				IAMProvider:      &iam.Provider{},
+				IdentityProvider: awslib.IdentityProvider{Logger: log},
+>>>>>>> 6bf970af (fix panic due to missing logger of aws IdentityProvider (#2095))
 				AccountProvider:  awslib.AccountProvider{},
 			}, nil
 		}
 
 		return &AWS{
-			IdentityProvider: awslib.IdentityProvider{},
+			IdentityProvider: awslib.IdentityProvider{Logger: log},
 		}, nil
 	case config.CIS_EKS:
 		return &EKS{
 			AWSCfgProvider:         awslib.ConfigProvider{MetadataProvider: awslib.Ec2MetadataProvider{}},
-			AWSIdentityProvider:    awslib.IdentityProvider{},
+			AWSIdentityProvider:    awslib.IdentityProvider{Logger: log},
 			AWSMetadataProvider:    awslib.Ec2MetadataProvider{},
 			EKSClusterNameProvider: awslib.EKSClusterNameProvider{},
 			ClientProvider:         k8s.ClientGetter{},
@@ -73,7 +78,8 @@ func GetStrategy(cfg *config.Config) (Strategy, error) {
 	case config.CIS_AZURE:
 		return &Azure{
 			cfgProvider:         &azure_auth.ConfigProvider{AuthProvider: &azure_auth.AzureAuthProvider{}},
-			providerInitializer: &azurelib.ProviderInitializer{}}, nil
+			providerInitializer: &azurelib.ProviderInitializer{},
+		}, nil
 	}
 	return nil, fmt.Errorf("unknown benchmark: '%s'", cfg.Benchmark)
 }
