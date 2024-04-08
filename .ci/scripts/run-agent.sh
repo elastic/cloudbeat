f685a484f4e82b2740b659edf3b27f6e9d5be6f4
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2024-04-08 10:12:11 +0300

    add version 8.12

--- .ci/scripts/run-agent.sh
+++ .ci/scripts/run-agent.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ELASTIC_AGENT_VERSION=$1
+PACKAGE_VERSION=$2
 
 curl -L -O "https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64.tar.gz"
 tar xzvf "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64.tar.gz"
@@ -8,11 +9,11 @@ tar xzvf "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64.tar.gz"
 # For debugging purposes, copy the original configuration file
 cp "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent.yml" "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent-orig.yml"
 # Copy customized configuration file
-cp ./tests/test_environments/agents/elastic-agent-8-13.yml "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent.yml"
+cp ./tests/test_environments/agents/elastic-agent-standalone.yml "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent.yml"
 
 # Array of placeholders and corresponding environment variable names
-placeholders=("ES_HOST" "ES_USERNAME" "ES_PASSWORD" "AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY" "GOOGLE_APPLICATION_CREDENTIALS" "AZURE_CLIENT_ID" "AZURE_TENANT_ID" "AZURE_CLIENT_SECRET")
-env_variables=("$ES_HOST" "$ES_USERNAME" "$ES_PASSWORD" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" "$GOOGLE_APPLICATION_CREDENTIALS" "$AZURE_CLIENT_ID" "$AZURE_TENANT_ID" "$AZURE_CLIENT_SECRET")
+placeholders=("ES_HOST" "ES_USERNAME" "ES_PASSWORD" "AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY" "GOOGLE_APPLICATION_CREDENTIALS" "AZURE_CLIENT_ID" "AZURE_TENANT_ID" "AZURE_CLIENT_SECRET" "PACKAGE_VERSION")
+env_variables=("$ES_HOST" "$ES_USERNAME" "$ES_PASSWORD" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" "$GOOGLE_APPLICATION_CREDENTIALS" "$AZURE_CLIENT_ID" "$AZURE_TENANT_ID" "$AZURE_CLIENT_SECRET" "$PACKAGE_VERSION")
 
 agent_config="./elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent.yml"
 # Iterate through each placeholder and replace it with the corresponding environment variable value
@@ -20,7 +21,6 @@ for ((i = 0; i < ${#placeholders[@]}; i++)); do
     placeholder="${placeholders[$i]}"
     env_variable="${env_variables[$i]}"
     awk -v placeholder="$placeholder" -v env_variable="$env_variable" '{gsub("\\${" placeholder "}", env_variable)} 1' "$agent_config" >tmp && mv tmp "$agent_config"
-    # sed -i "s|\${placeholder}|${env_variable}|" ./elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64/elastic-agent.yml
 done
 
 cd "elastic-agent-${ELASTIC_AGENT_VERSION}-linux-x86_64" || exit
