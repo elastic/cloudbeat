c8553c6f47818d3f423a4d24a294eb4c71716a30
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2024-06-17 15:19:44 +0300

    update script and wf

--- .ci/scripts/get_expired_envs.sh
+++ .ci/scripts/get_expired_envs.sh
@@ -2,31 +2,39 @@
 
 # Function to check if a file exists in a folder
 file_exists() {
-    local bucket_name="$1"
-    local bucket_key="$2"
+    local bucket_path="$1"
+    local bucket_exists
     # Check if the file exists in the bucket
-    if aws s3api head-object --bucket "$bucket_name" --key "$bucket_key" >/dev/null 2>&1; then
-        return 0
+    bucket_exists=$(aws s3 ls "$bucket_path")
+    if [ -z "$bucket_exists" ]; then
+        return 1
     fi
-    return 1
+    return 0
 }
 
 # Get all folders from the specific S3 bucket
 s3_bucket="tf-state-bucket-test-infra"
-folders=$(aws s3 ls "s3://$s3_bucket" | awk '{print $2}')
+folders=$(aws s3 ls "s3://${s3_bucket}" | awk '{print $2}')
 
 # JSON array to store deployment names
-deployment_json='[]'
+deployments_json='[]'
+
+# Boolean variable to track if an expired environment was found
+expired_env_found=false
 
 # Iterate over each folder
 for folder in $folders; do
     # Check if env_config.json file exists
-    if ! file_exists "$s3_bucket" "$folder/env_config.json"; then
-        echo "env_config.json file does not exist in $folder"
+    if ! file_exists "s3://${s3_bucket}/${folder}env_config.json"; then
+        # TODO: To consider if we want to log this message
+        # echo "env_config.json file does not exist in $folder"
         continue
     fi
 
-    file_content=$(aws s3 cp s3://"$s3_bucket"/"$folder"/env_config.json -)
+    # TODO: To consider if we want to log this message
+    # echo "Reading env_config.json file from $folder"
+
+    file_content=$(aws s3 cp "s3://${s3_bucket}/${folder}env_config.json" -)
 
     # Read expiration date from env_config.json
     expiration=$(echo "$file_content" | jq -r '.expiration')
@@ -36,11 +44,19 @@ for folder in $folders; do
     if [[ ! "$expiration" > "$current_date" ]]; then
         # Extract the deployment_name field using jq
         deployment_name=$(echo "$file_content" | jq -r '.deployment_name')
-
+        # TODO: To consider if we want to log this message
+        echo "Environment $deployment_name is expired."
         # Add deployment name to JSON object
-        deployment_json=$(echo "$deployment_json" | jq --arg value "$deployment_name" '. += [{"deployment_name": $value}]')
+        deployments_json=$(echo "$deployments_json" | jq --arg value "$deployment_name" '. += [{"deployment_name": $value}]')
+
+        # Set found to true
+        expired_env_found=true
+    else
+        # TODO: To consider if we want to log this message
+        echo "Environment $(echo "$file_content" | jq -r '.deployment_name') is not expired yet."
     fi
 done
 
-# Print the deployment names to GITHUB_OUTPUT
-echo "deployments=$(echo "$deployment_json" | jq .)" >>"$GITHUB_OUTPUT"
+# Print the deployment names and found status to GITHUB_OUTPUT
+echo "deployments=$(echo "$deployments_json" | jq .)" >>"$GITHUB_OUTPUT"
+echo "expired_env_found=$expired_env_found" >>"$GITHUB_OUTPUT"
