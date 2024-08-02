6f1675f2ec537d9ec1b80866ca1dab86a92e7174
kubasobon <wtty.fool@gmail.com>
2024-08-02 11:30:50 +0200

    refactor code and move it to strategy file

--- internal/flavors/asset_inventory.go
+++ internal/flavors/asset_inventory.go
@@ -20,20 +20,14 @@ package flavors
 import (
 	"context"
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/elastic/beats/v7/libbeat/beat"
 	agentconfig "github.com/elastic/elastic-agent-libs/config"
 	"github.com/elastic/elastic-agent-libs/logp"
 
 	"github.com/elastic/cloudbeat/internal/config"
+	"github.com/elastic/cloudbeat/internal/flavors/assetinventory"
 	"github.com/elastic/cloudbeat/internal/inventory"
-	"github.com/elastic/cloudbeat/internal/inventory/awsfetcher"
-	"github.com/elastic/cloudbeat/internal/inventory/azurefetcher"
-	"github.com/elastic/cloudbeat/internal/resources/providers/awslib"
-	"github.com/elastic/cloudbeat/internal/resources/providers/azurelib"
-	azure_auth "github.com/elastic/cloudbeat/internal/resources/providers/azurelib/auth"
 )
 
 type assetInventory struct {
@@ -53,41 +47,24 @@ func newAssetInventoryFromCfg(b *beat.Beat, cfg *config.Config) (*assetInventory
 	logger := logp.NewLogger("asset_inventory")
 	ctx, cancel := context.WithCancel(context.Background())
 
-	var fetchers []inventory.AssetFetcher
-	var err error
-
-	switch cfg.AssetInventoryProvider {
-	case config.ProviderAWS:
-		fetchers, err = initAwsFetchers(ctx, cfg, logger)
-	case config.ProviderAzure:
-		fetchers, err = initAzureFetchers(ctx, cfg, logger)
-	case config.ProviderGCP:
-		err = fmt.Errorf("GCP branch not implemented")
-	case "":
-		err = fmt.Errorf("missing config.v1.asset_inventory_provider setting")
-	default:
-		err = fmt.Errorf("unsupported Asset Inventory provider %q", cfg.AssetInventoryProvider)
-	}
-	if err != nil {
-		cancel()
-		return nil, err
-	}
-	logger.Infof("Creating %s AssetInventory", strings.ToUpper(cfg.AssetInventoryProvider))
-
-	publisherClient, err := NewClient(b.Publisher, cfg.Processors)
+	beatClient, err := NewClient(b.Publisher, cfg.Processors)
 	if err != nil {
 		cancel()
 		return nil, fmt.Errorf("failed to init client: %w", err)
 	}
 
-	now := func() time.Time { return time.Now() } //nolint:gocritic
-	newAssetInventory := inventory.NewAssetInventory(logger, fetchers, publisherClient, now)
-	publisher := NewPublisher(logger, flushInterval, eventsThreshold, publisherClient)
+	strategy := assetinventory.GetStrategy(logger, cfg)
+	newAssetInventory, err := strategy.NewAssetInventory(ctx, beatClient)
+	if err != nil {
+		cancel()
+		return nil, err
+	}
 
+	publisher := NewPublisher(logger, flushInterval, eventsThreshold, beatClient)
 	return &assetInventory{
 		flavorBase: flavorBase{
 			ctx:       ctx,
-			client:    publisherClient,
+			client:    beatClient,
 			cancel:    cancel,
 			publisher: publisher,
 			config:    cfg,
@@ -95,36 +72,6 @@ func newAssetInventoryFromCfg(b *beat.Beat, cfg *config.Config) (*assetInventory
 		},
 		assetInventory: newAssetInventory,
 	}, nil
-}
-
-func initAwsFetchers(ctx context.Context, cfg *config.Config, logger *logp.Logger) ([]inventory.AssetFetcher, error) {
-	awsConfig, err := awslib.InitializeAWSConfig(cfg.CloudConfig.Aws.Cred)
-	if err != nil {
-		return nil, err
-	}
-
-	idProvider := awslib.IdentityProvider{Logger: logger}
-	awsIdentity, err := idProvider.GetIdentity(ctx, *awsConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return awsfetcher.New(logger, awsIdentity, *awsConfig), nil
-}
-
-func initAzureFetchers(_ context.Context, cfg *config.Config, logger *logp.Logger) ([]inventory.AssetFetcher, error) {
-	cfgProvider := &azure_auth.ConfigProvider{AuthProvider: &azure_auth.AzureAuthProvider{}}
-	azureConfig, err := cfgProvider.GetAzureClientConfig(cfg.CloudConfig.Azure)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize azure config: %w", err)
-	}
-	initializer := &azurelib.ProviderInitializer{}
-	provider, err := initializer.Init(logger, *azureConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize azure config: %w", err)
-	}
-
-	return azurefetcher.New(logger, provider, azureConfig), nil
 }
 
 func (bt *assetInventory) Run(*beat.Beat) error {
