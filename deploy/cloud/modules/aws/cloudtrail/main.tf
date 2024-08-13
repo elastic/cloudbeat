b6754e5082bd6f45cb7b3cf2af8bea823e1ebdc0
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2024-08-13 12:11:12 +0300

    fix format

--- deploy/cloud/modules/aws/cloudtrail/main.tf
+++ deploy/cloud/modules/aws/cloudtrail/main.tf
@@ -1,18 +1,18 @@
 locals {
   s3_bucket_name = "tf-test-envs-cloudtrail-logs"
   common_tags = {
-    division   = "${var.division}"
-    org        = "${var.org}"
-    team       = "${var.team}"
-    project    = "${var.project}"
-    owner      = "${var.owner}"
+    division = "${var.division}"
+    org      = "${var.org}"
+    team     = "${var.team}"
+    project  = "${var.project}"
+    owner    = "${var.owner}"
   }
 }
 
 resource "aws_s3_bucket" "cloudtrail" {
-  bucket = var.s3_bucket_name
+  bucket        = var.s3_bucket_name
   force_destroy = true
-  tags = local.common_tags
+  tags          = local.common_tags
 }
 
 resource "aws_kms_key" "cloudtrail" {
@@ -21,27 +21,27 @@ resource "aws_kms_key" "cloudtrail" {
   policy = jsonencode({
     Version = "2012-10-17",
     Id      = "key-default-1",
-    Statement: [
+    Statement : [
       {
-        Sid       = "Enable IAM User Permissions",
-        Effect    = "Allow",
+        Sid    = "Enable IAM User Permissions",
+        Effect = "Allow",
         Principal = {
           AWS = "arn:aws:iam::${var.aws_account_id}:root"
         },
-        Action    = "kms:*",
-        Resource  = "*"
+        Action   = "kms:*",
+        Resource = "*"
       },
       {
-        Sid       = "Allow CloudTrail to encrypt logs",
-        Effect    = "Allow",
+        Sid    = "Allow CloudTrail to encrypt logs",
+        Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
-        Action    = [
+        Action = [
           "kms:GenerateDataKey*",
           "kms:Decrypt"
         ],
-        Resource  = "*",
+        Resource = "*",
         Condition = {
           StringLike = {
             "kms:EncryptionContext:aws:cloudtrail:arn" = "arn:aws:cloudtrail:*:${var.aws_account_id}:trail/*"
@@ -49,13 +49,13 @@ resource "aws_kms_key" "cloudtrail" {
         }
       },
       {
-        Sid       = "Allow CloudTrail to describe key",
-        Effect    = "Allow",
+        Sid    = "Allow CloudTrail to describe key",
+        Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
-        Action    = "kms:DescribeKey",
-        Resource  = "*"
+        Action   = "kms:DescribeKey",
+        Resource = "*"
       }
     ]
   })
@@ -71,7 +71,6 @@ resource "aws_kms_alias" "cloudtrail" {
 resource "aws_cloudtrail" "main" {
   name                          = var.cloudtrail_name
   s3_bucket_name                = aws_s3_bucket.cloudtrail.bucket
-  # s3_bucket_name                = "tf-test-envs-cloudtrail-logs" # we will use predefined bucket name
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_log_file_validation    = true
@@ -147,7 +146,7 @@ resource "aws_s3_bucket_policy" "cloudtrail" {
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         }
-        Action = "s3:GetBucketAcl"
+        Action   = "s3:GetBucketAcl"
         Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail.bucket}"
       },
       {
@@ -155,7 +154,7 @@ resource "aws_s3_bucket_policy" "cloudtrail" {
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         }
-        Action = "s3:PutObject"
+        Action   = "s3:PutObject"
         Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail.bucket}/AWSLogs/${var.aws_account_id}/*"
         Condition = {
           StringEquals = {
