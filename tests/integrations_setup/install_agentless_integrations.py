cc143d416bf8190b9902bd27af31581b53e6c84b
kubasobon <wtty.fool@gmail.com>
2024-09-10 17:06:36 +0200

    isort

--- tests/integrations_setup/install_agentless_integrations.py
+++ tests/integrations_setup/install_agentless_integrations.py
@@ -10,14 +10,13 @@ The following steps are performed:
 import json
 
 import configuration_fleet as cnfg
+from fleet_api.package_policy_api import create_cspm_integration
 from loguru import logger
 from package_policy import (
     generate_package_policy,
     generate_policy_template,
     generate_random_name,
 )
-
-from fleet_api.package_policy_api import create_cspm_integration
 
 AGENT_POLICY_ID = "agentless"
 
