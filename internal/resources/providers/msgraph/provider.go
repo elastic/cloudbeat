69bd009a66d84dce5859e842771555c9e8489d69
kubasobon <wtty.fool@gmail.com>
2024-10-18 13:08:41 +0200

    finish internal service principal provider

--- internal/resources/providers/msgraph/provider.go
+++ internal/resources/providers/msgraph/provider.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/elastic/cloudbeat/internal/resources/providers/azurelib/auth"
-	"github.com/elastic/cloudbeat/internal/resources/providers/azurelib/inventory"
 
 	"github.com/elastic/elastic-agent-libs/logp"
 	graph "github.com/microsoftgraph/msgraph-sdk-go"
@@ -14,7 +13,9 @@ import (
 	"github.com/microsoftgraph/msgraph-sdk-go/serviceprincipals"
 )
 
-type ProviderAPI interface{}
+type ProviderAPI interface {
+	ListServicePrincipals(context.Context) ([]*models.ServicePrincipal, error)
+}
 
 type provider struct {
 	log    *logp.Logger
@@ -23,9 +24,9 @@ type provider struct {
 	}
 }
 
+// Second argument is scopes. Leave nil, then it selects default; Adjust if in trouble
+// Docs: https://learn.microsoft.com/en-us/graph/sdks/create-client?from=snippets&tabs=go
 func NewProvider(log *logp.Logger, azureConfig auth.AzureFactoryConfig) (ProviderAPI, error) {
-	// Second argument is scopes. Leave nil, then it selects default; Adjust if in trouble
-	// Docs: https://learn.microsoft.com/en-us/graph/sdks/create-client?from=snippets&tabs=go
 	c, err := graph.NewGraphServiceClientWithCredentials(azureConfig.Credentials, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error creating MS Graph client: %w", err)
@@ -39,12 +40,11 @@ func NewProvider(log *logp.Logger, azureConfig auth.AzureFactoryConfig) (Provide
 	return p, nil
 }
 
-// TODO(kuba): replace return with []AzureAsset
 // Docs:
 // - https://github.com/microsoftgraph/msgraph-sdk-go
 // - https://learn.microsoft.com/en-us/graph/api/serviceprincipal-list?view=graph-rest-beta&tabs=go
 // - https://learn.microsoft.com/en-us/graph/sdks/paging?tabs=go
-func (p *provider) ListServicePrincipals(ctx context.Context) ([]inventory.AzureAsset, error) {
+func (p *provider) ListServicePrincipals(ctx context.Context) ([]*models.ServicePrincipal, error) {
 	requestConfig := &serviceprincipals.ServicePrincipalsRequestBuilderGetRequestConfiguration{}
 
 	response, err := p.client.ServicePrincipals().Get(ctx, requestConfig)
@@ -58,23 +58,13 @@ func (p *provider) ListServicePrincipals(ctx context.Context) ([]inventory.Azure
 		models.CreateServicePrincipalCollectionResponseFromDiscriminatorValue,
 	)
 
-	assets := []inventory.AzureAsset{}
+	items := []*models.ServicePrincipal{}
 	err = pageIterator.Iterate(ctx, func(pageItem *models.ServicePrincipal) bool {
-		asset, err := p.transformServicePrincipal(pageItem)
-		if err != nil {
-			p.log.Errorf("error transforming Service Principal: %v", err)
-			return true
-		}
-		assets = append(assets, asset)
+		items = append(items, pageItem)
 		return true // to continue the iteration
 	})
 	if err != nil {
 		p.log.Errorf("error iterating over Service Principals: %v", err)
 	}
-	return assets, nil
-}
-
-func (p *provider) transformServicePrincipal(item *models.ServicePrincipal) (inventory.AzureAsset, error) {
-
-	return nil, nil
+	return items, nil
 }
