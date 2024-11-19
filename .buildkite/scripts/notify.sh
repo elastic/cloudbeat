de0be835bb3ec35a1d727e7d8396d195246e2f39
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2024-11-19 11:14:05 +0200

    modify bk pipeline

---
+++ .buildkite/scripts/notify.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Define a default Slack user
+# default_slack_user="cloudbeat-eng-team"
+default_slack_user="Dima Gurevich"
+
+# Check if BUILDKITE_BUILD_AUTHOR is set, defaulting to an empty string
+build_author="${BUILDKITE_BUILD_AUTHOR:-}"
+
+# Map GitHub usernames to Slack usernames using a case statement
+case "$build_author" in
+  "gurevichdmitry")
+    slack_user="Dima Gurevich"
+    ;;
+  "newuser")
+    slack_user="slack user2"
+    ;;
+  *)
+    slack_user="$default_slack_user"
+    ;;
+esac
+
+# Output the YAML configuration
+cat <<EOF
+steps:
+  - label: "Slack notification"
+    command: echo "Notification test"
+    notify:
+      - slack:
+          channels:
+            - "#cloud-sec-ci"
+          message: "Test: ping @$slack_user"
+EOF
