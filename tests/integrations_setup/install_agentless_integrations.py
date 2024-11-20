f2a42abeb91710d694a24f5b2579588cfc043932
Amir Ben Nun <amir.benun7@gmail.com>
2024-11-20 12:17:46 +0200

    Revert "apply gcp"
    This reverts commit 718334a4a6868fd749df16e162c8398627758cce.

--- tests/integrations_setup/install_agentless_integrations.py
+++ tests/integrations_setup/install_agentless_integrations.py
@@ -66,6 +66,7 @@ def generate_gcp_integration_data():
         "posture": "cspm",
         "deployment": "gcp",
         "vars": {
+            "setup_access": "manual",
             "gcp.account_type": "single-account",
             "gcp.credentials.type": "credentials-json",
             "gcp.credentials.json": credentials_json,
@@ -77,7 +78,6 @@ if __name__ == "__main__":
     integrations = [
         generate_aws_integration_data(),
         generate_azure_integration_data(),
-        generate_gcp_integration_data(),
     ]
     cspm_template = generate_policy_template(
         cfg=cnfg.elk_config,
