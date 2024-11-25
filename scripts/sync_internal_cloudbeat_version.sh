ae7f9b9a7bd1e742083ad51445896d372171e40d
kubasobon <wtty.fool@gmail.com>
2024-11-25 14:37:22 +0100

    update sync_script

--- scripts/sync_internal_cloudbeat_version.sh
+++ scripts/sync_internal_cloudbeat_version.sh
@@ -15,23 +15,14 @@ set_hermit_cloudbeat_version() {
     sed -E -i '' "s/CLOUDBEAT_VERSION\": \".*\"/CLOUDBEAT_VERSION\": \"$CLOUDBEAT_VERSION\"/g" $HERMIT_FILE
 }
 
-create_pr_if_different() {
+commit_if_different() {
     if git diff --quiet --exit-code $HERMIT_FILE; then
         echo "No changes to $HERMIT_FILE; I'm done"
         return
     fi
-    echo "Versions changed, creating a GitHub PR"
-    git checkout -b "sync-hermit-version-$(date +%s)"
+    echo "Versions changed, commiting changes"
     git add $HERMIT_FILE
     git commit -m "bump CLOUDBEAT_VERSION in $HERMIT_FILE to $CLOUDBEAT_VERSION"
-    git push -u origin
-
-    echo "Updates CLOUDBEAT_VERSION in $HERMIT_FILE to $CLOUDBEAT_VERSION." >pr_body
-    gh pr create --title "Bump cloudbeat version in hermit.hcl to $CLOUDBEAT_VERSION" \
-        --body-file pr_body \
-        --base "$BASE_BRANCH" \
-        --label "backport-skip"
-    rm pr_body
 }
 
 find_current_cloudbeat_version
