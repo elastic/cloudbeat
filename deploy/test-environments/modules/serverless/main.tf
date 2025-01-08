d3fb86a59d6264faa8f34900aa4c2a909dc0d0ad
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2025-01-08 16:43:19 +0200

    update script

--- deploy/test-environments/modules/serverless/main.tf
+++ deploy/test-environments/modules/serverless/main.tf
@@ -10,7 +10,7 @@ locals {
   for i in {1..20}; do
     response=$(curl -s -H "Content-type: application/json" -H "Authorization: ApiKey ${var.ec_apikey}" "${var.ec_url}/api/v1/serverless/projects/security/${restapi_object.ec_project.api_data.id}/status" 2>/dev/null)
     phase=$(echo $response | jq -r '.phase')
-    if [ "$phase" == "initialized" ]; then
+    if [ "$phase" = "initialized" ]; then
       echo "Project is available!"
       exit 0
     else
