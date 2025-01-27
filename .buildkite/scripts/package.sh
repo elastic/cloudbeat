88ce4435dd097cbee2bbc8cd227e1b993f5b192e
Kostas Stamatakis <kostas.stamatakis@elastic.co>
2025-01-27 12:38:26 +0200

    [buildkite]: get version qualifier from google bucket

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -8,7 +8,9 @@ source ./bin/activate-hermit
 CLOUDBEAT_VERSION=$(grep defaultBeatVersion version/version.go | cut -d'=' -f2 | tr -d '" ')
 PYTHON_BIN=./build/ve/$(go env GOOS)/bin
 PYTHON=$PYTHON_BIN/python
-VERSION_QUALIFIER="${VERSION_QUALIFIER:-""}"
+
+source .buildkite/scripts/qualifier.sh
+echo "VERSION_QUALIFIER: ${VERSION_QUALIFIER}"
 
 if [ "$WORKFLOW" = "snapshot" ]; then
     export SNAPSHOT="true"
@@ -22,7 +24,7 @@ mage package
 
 CSV_FILE="build/dependencies-${CLOUDBEAT_VERSION}"
 [ -n "${SNAPSHOT+x}" ] && CSV_FILE+="-SNAPSHOT"
-if [[ -n "$VERSION_QUALIFIER" ]]; then
+if [[ -n "${VERSION_QUALIFIER}" ]]; then
     CSV_FILE+="-${VERSION_QUALIFIER}"
 fi
 
---
+++ .buildkite/scripts/qualifier.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# this file should be sourced from inside the package and publish script.
+
+fetch_elastic_qualifier() {
+    local branch="${1}"
+    local url="https://storage.googleapis.com/dra-qualifier/${branch}"
+    local qualifier=""
+    if curl -sf -o /dev/null "${url}"; then
+        qualifier=$(curl -s "${url}")
+    fi
+    echo "${qualifier}"
+}
+
+# If the VERSION_QUALIFIER is already set (e.g. buildkite custom run), use that
+# else try to fetch from google bucket for the current branch
+if [ -z "${VERSION_QUALIFIER+x}" ]; then
+    # VERSION_QUALIFIER is not set, get from bucket
+    VERSION_QUALIFIER="$(fetch_elastic_qualifier "${BUILDKITE_BRANCH}")"
+fi
