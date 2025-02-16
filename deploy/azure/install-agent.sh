336543fabf7e98f803372c7ef251c5e603ddd9ce
Dmitry Gurevich <dmitry.gurevich@elastic.co>
2025-02-16 17:09:17 +0200

    Update install-agent.sh script (#3023)
    * update az install scripts
    
    * add for debug and testing
    
    * revert debug template change
    
    (cherry picked from commit 83197406158b8f8f32b07ba79fa99db52248bbf3)
    
    # Conflicts:
    #	deploy/azure/install-agent-dev.sh
    #	deploy/azure/install-agent.sh

--- deploy/azure/install-agent.sh
+++ deploy/azure/install-agent.sh
@@ -15,10 +15,20 @@ ElasticArtifactServer=${2:?$usage}
 FleetUrl=${3:?$usage}
 EnrollmentToken=${4:?$usage}
 
+install_servers_opt=""
+major_version=$(echo "${ElasticAgentVersion}" | cut -d'.' -f1)
+if [ "$major_version" -ge 9 ]; then
+    install_servers_opt="--install-servers"
+fi
+
 ElasticAgentArtifact="elastic-agent-$ElasticAgentVersion-linux-x86_64"
 curl -L -O "${ElasticArtifactServer}/$ElasticAgentArtifact.tar.gz"
 tar xzf "${ElasticAgentArtifact}.tar.gz"
 cd "${ElasticAgentArtifact}"
+<<<<<<< HEAD
 sudo ./elastic-agent install --non-interactive --url="${FleetUrl}" --enrollment-token="${EnrollmentToken}"
+=======
+sudo ./elastic-agent install --non-interactive --url="${FleetUrl}" --enrollment-token="${EnrollmentToken}" ${install_servers_opt:+$install_servers_opt}
+>>>>>>> 83197406 (Update install-agent.sh script (#3023))
 
 wait
