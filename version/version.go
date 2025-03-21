1636b4d7bb1fe3204828c53d9ff28836db4227a4
elasticmachine <infra-root+elasticmachine@elastic.co>
2025-03-21 13:25:13 +0000

    [Release] update version

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@
 package version
 
 // name matches github.com/elastic/beats/v7/dev-tools/mage/settings.go parseBeatVersion
-const defaultBeatVersion = "8.16.6"
+const defaultBeatVersion = "8.16.7"
 
 // Version represents version information for a package
 type Version struct {
