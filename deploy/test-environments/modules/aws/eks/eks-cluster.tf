ebe469bf1270c8f30712456160ab40cab323862d
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2025-03-27 10:22:44 +0200

    update format

--- deploy/test-environments/modules/aws/eks/eks-cluster.tf
+++ deploy/test-environments/modules/aws/eks/eks-cluster.tf
@@ -4,8 +4,8 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   cluster_version = "1.32"
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
 
   enable_irsa                 = true
   create_cloudwatch_log_group = false
