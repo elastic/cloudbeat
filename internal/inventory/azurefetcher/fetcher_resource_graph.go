d936fd99cf2bfaea842c7b8db68926861722f646
Kuba Soboń <wtty.fool@gmail.com>
2025-07-29 13:27:51 +0200

    [Asset Inventory][Azure] Add missing `cloud.*` section information (#3470)
    * update Azure cloud.* section
    * fix test cases
    
    (cherry picked from commit 04b20493164d15c596b5571699b78f6cbfe62857)
    
    # Conflicts:
    #	go.mod
    #	go.sum
    #	internal/inventory/azurefetcher/fetcher_resource_graph.go
    #	internal/inventory/azurefetcher/fetcher_resource_graph_test.go
    #	internal/inventory/azurefetcher/fetcher_storage.go
    #	internal/inventory/azurefetcher/fetcher_storage_test.go

--- internal/inventory/azurefetcher/fetcher_resource_graph.go
+++ internal/inventory/azurefetcher/fetcher_resource_graph.go
@@ -90,9 +90,30 @@ func (f *resourceGraphFetcher) fetch(ctx context.Context, resourceName, resource
 			inventory.WithRawAsset(item),
 			inventory.WithCloud(inventory.Cloud{
 				Provider:    inventory.AzureCloudProvider,
+				Region:      item.Location,
 				AccountID:   item.TenantId,
+				ProjectID:   item.SubscriptionId,
 				ServiceName: "Azure",
 			}),
 		)
+<<<<<<< HEAD
+=======
+
+		if resourceType == azurelib.VirtualMachineAssetType {
+			vmProperties := tryUnpackingVMProperties(item.Properties)
+			if vmProperties != nil {
+				asset.Host = &inventory.Host{
+					ID:   item.Id,
+					Name: vmProperties.Extended.InstanceView.ComputerName,
+					Type: vmProperties.HardwareProfile.VmSize,
+				}
+				asset.Cloud.MachineType = vmProperties.HardwareProfile.VmSize
+			}
+			asset.Cloud.InstanceID = item.Id
+			asset.Cloud.InstanceName = item.Name
+		}
+
+		assetChan <- asset
+>>>>>>> 04b20493 ([Asset Inventory][Azure] Add missing `cloud.*` section information (#3470))
 	}
 }
