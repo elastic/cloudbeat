229d4534fc67cc6dd06ea6f036530067c83907db
Dmitry Gurevich <dmitry.gurevich@elastic.co>
2026-07-01 17:23:37 +0300

    update experimental features

--- deploy/test-environments/elk-stack/main.tf
+++ deploy/test-environments/elk-stack/main.tf
@@ -67,8 +67,7 @@ module "ec_deployment" {
     "apm"           = ""
   }
 
-  kibana_enable_security_solution_experimental = var.kibana_security_solution_experimental
-  kibana_enable_entity_analytics_settings      = var.kibana_enable_entity_analytics_settings
+  kibana_enable_entity_analytics_settings = var.kibana_enable_entity_analytics_settings
   kibana_instance_size                         = var.kibana_instance_size
 }
 
