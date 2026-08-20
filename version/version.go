a7f6720eea46b6ecac94fbae503c8d29e9666bb5
Cloud Security Machine <cloudsecmachine@users.noreply.github.com>
2026-08-20 16:59:52 +0000

    Bump to 9.5.3

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@
 package version
 
 // name matches github.com/elastic/beats/v7/dev-tools/mage/settings.go parseBeatVersion
-const defaultBeatVersion = "9.5.2"
+const defaultBeatVersion = "9.5.3"
 
 var qualifier = ""
 
